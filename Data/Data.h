66d9610d37e118d4a81848a2998a85c8bee8fe55
Joel Reis <joelhreis@gmail.com>
2018-05-17 14:21:01 -0400

    Created 'Data' module.

---
+++ Data/Data.h
@@ -0,0 +1,19 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Joel Reis on 5/16/18.
+//  Copyright © 2018 Mozilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
