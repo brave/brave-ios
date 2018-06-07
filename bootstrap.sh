507775e7581fa71ccc4443367140f1b192b600c7
Joel Reis <joelhreis@gmail.com>
2018-06-07 15:36:31 -0400

    Updated xcconfig files to work better for local changes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,4 +33,29 @@ npm install
 npm run build
 
 # Do not commit changes to local file, this allows signing without causing git changes.
-git update-index --assume-unchanged Client/Configuration/Local.xcconfig
+# git update-index --assume-unchanged Client/Configuration/Local.xcconfig
+
+CONFIG_PATH="Client/Configuration/"
+if [ ! -d /$CONFIG_PATH/Local ]; then
+  echo "File not found!"
+fi
+
+# Sets up local configurations from the tracked .template files
+
+# Checking the `Local` Directory
+CONFIG_PATH="Client/Configuration"
+if [ ! -d "$CONFIG_PATH/Local/" ]; then
+  echo "Creating 'Local' directory"
+
+  (cd $CONFIG_PATH && mkdir Local)
+fi
+
+# Copying over any necessary files into `Local`
+for CONFIG_FILE_NAME in BundleId DevTeam
+do
+  CONFIG_FILE=$CONFIG_FILE_NAME.xcconfig
+  echo $CONFIG_PATH $CONFIG_FILE
+  (cd $CONFIG_PATH \
+    && cp -n Local.templates/$CONFIG_FILE Local/$CONFIG_FILE \
+  )
+done
