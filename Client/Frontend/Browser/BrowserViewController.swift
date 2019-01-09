8fc0af851937935f37ae92b1fcc81719662ee907
Danish Jafri <danishjafri88@gmail.com>
2019-01-10 03:24:00 +0530

    The footer view will never be nil but is initialised in viewdidload so treating it as non-nil while declaring it as unwrapped optional. Removed old animation code fix which became redundant after update

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -667,11 +667,7 @@ class BrowserViewController: UIViewController {
             webViewContainerTopOffset = make.top.equalTo(readerModeBar?.snp.bottom ?? self.header.snp.bottom).constraint
 
             let findInPageHeight = (findInPageBar == nil) ? 0 : UIConstants.ToolbarHeight
-            if let footerView = self.footer {
-                make.bottom.equalTo(footerView.snp.top).offset(-findInPageHeight)
-            } else {
-                make.bottom.equalTo(self.view).offset(-findInPageHeight)
-            }
+            make.bottom.equalTo(self.footer.snp.top).offset(-findInPageHeight)
         }
 
         // Setup the bottom toolbar
@@ -694,7 +690,7 @@ class BrowserViewController: UIViewController {
             
             make.left.right.equalTo(self.view)
             if self.homePanelIsInline {
-                make.bottom.equalTo(self.footer?.snp.top ?? self.view.snp.bottom)
+                make.bottom.equalTo(self.footer.snp.top)
             } else {
                 make.bottom.equalTo(self.view.snp.bottom)
             }
@@ -705,10 +701,8 @@ class BrowserViewController: UIViewController {
             make.width.equalTo(self.view.snp.width)
             if let keyboardHeight = keyboardState?.intersectionHeightForView(self.view), keyboardHeight > 0 {
                 make.bottom.equalTo(self.view).offset(-keyboardHeight)
-            } else if let footer = self.footer {
-                make.bottom.equalTo(footer.snp.top)
             } else {
-                make.bottom.equalTo(self.view)
+                make.bottom.equalTo(self.footer.snp.top)
             }
         }
     }
