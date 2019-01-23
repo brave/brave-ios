210bc138a7716b795a674b6931a24489693a3bb8
Danish Jafri <danishjafri88@gmail.com>
2019-01-23 23:31:43 +0530

    Formatted Indentation in cookie control.js

--- Client/Frontend/UserContent/UserScripts/CookieControl.js
+++ Client/Frontend/UserContent/UserScripts/CookieControl.js
@@ -2,18 +2,18 @@
 
 //Cookie should neither be saved nor accessed (local or session) when user has blocked all cookies.
 var $<cookie> = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie') ||
-Object.getOwnPropertyDescriptor(HTMLDocument.prototype, 'cookie');
+    Object.getOwnPropertyDescriptor(HTMLDocument.prototype, 'cookie');
 if ($<cookie> && $<cookie>.configurable) {
     Object.defineProperty(document, 'cookie', {
-                          get: function () {
-                          // Not returning null here as some websites don't have a check for cookie returning null, and may not behave properly
-                          return "";
-                          },
-                          set: function (val) {
-                          console.error("Access denied for 'cookie'")
-                          return;
-                          }
-                          });
+        get: function() {
+            // Not returning null here as some websites don't have a check for cookie returning null, and may not behave properly
+            return "";
+        },
+        set: function(val) {
+            console.error("Access denied for 'cookie'")
+            return;
+        }
+    });
 }
 
 
@@ -21,20 +21,20 @@ if ($<cookie> && $<cookie>.configurable) {
 var $<local> = Object.getOwnPropertyDescriptor(window, 'localStorage');
 if ($<local>) {
     Object.defineProperty(window, 'localStorage', {
-                          get: function () {
-                          console.error("Access denied for 'localStorage'")
-                          return null;
-                          },
-                          });
+        get: function() {
+            console.error("Access denied for 'localStorage'")
+            return null;
+        },
+    });
 }
 
 //Access to sessionStorage should be denied when user has blocked all Cookies.
 var $<session> = Object.getOwnPropertyDescriptor(window, 'sessionStorage');
 if ($<session>) {
     Object.defineProperty(window, 'sessionStorage', {
-                          get: function () {
-                          console.error("Access denied for 'sessionStorage'")
-                          return null;
-                          },
-                          });
+        get: function() {
+            console.error("Access denied for 'sessionStorage'")
+            return null;
+        },
+    });
 }
