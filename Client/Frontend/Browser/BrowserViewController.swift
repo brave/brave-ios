68e5fee07a7308f4f460962de1d2cea3c47b44f3
Michał Buczek <michal.buczek@protonmail.com>
2019-07-10 11:21:49 +0200

    Fix #1216: Opening a tab in private mode switches to PBM correctly.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -2845,6 +2845,7 @@ extension BrowserViewController: ToolbarUrlActionsDelegate {
     }
     
     func openInNewTab(_ url: URL, isPrivate: Bool) {
+        topToolbar.leaveOverlayMode()
         select(url, visitType: .unknown, action: .openInNewTab(isPrivate: isPrivate))
     }
     
