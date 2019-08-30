68e417b1f0dcc1d1efdaac4ae128126bce4e2a72
Joel Reis <joelhreis@gmail.com>
2019-08-29 15:03:01 -1000

    Ref #1391:iPad now defaults to show bookmark button.

--- Client/Application/ClientPreferences.swift
+++ Client/Application/ClientPreferences.swift
@@ -42,7 +42,7 @@ extension Preferences {
         /// Controls how the tab bar should be shown (or not shown)
         static let tabBarVisibility = Option<Int>(key: "general.tab-bar-visiblity", default: TabBarVisibility.always.rawValue)
         /// Specifies whether the bookmark button is present on toolbar
-        static let bookmarkToolbarVisibility = Option<Bool>(key: "general.bookmark-toolbar-visibility", default: false)
+        static let bookmarkToolbarVisibility = Option<Bool>(key: "general.bookmark-toolbar-visibility", default: UIDevice.isIpad)
         /// Sets Desktop UA for iPad by default (iOS 13+ & iPad only)
         static let alwaysRequestDesktopSite = Option<Bool>(key: "general.always-request-desktop-site", default: UIDevice.isIpad)
         
