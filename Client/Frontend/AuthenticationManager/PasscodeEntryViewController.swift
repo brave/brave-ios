448b97963a26dd7698bbfc28b86aaea07e8797ca
quang.ha <quangDecember@gmail.com>
2020-01-11 11:35:26 +0800

    removing the Previews

--- Client/Frontend/AuthenticationManager/PasscodeEntryViewController.swift
+++ Client/Frontend/AuthenticationManager/PasscodeEntryViewController.swift
@@ -122,18 +122,3 @@ extension PasscodeEntryViewController: PasscodeInputViewDelegate {
     }
 }
 
-#if canImport(SwiftUI) && MOZ_CHANNEL_FENNEC
-import SwiftUI
-@available(iOS 13, *)
-struct PasscodeEntryPreview: PreviewProvider {
-    static var previews: some View {
-        UIViewControllerPreview {
-            let v = PasscodeEntryViewController()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
-                v.displayError(String.localizedStringWithFormat(Strings.AuthenticationMaximumAttemptsReached, 2))
-            })
-            return v
-        }
-    }
-}
-#endif
