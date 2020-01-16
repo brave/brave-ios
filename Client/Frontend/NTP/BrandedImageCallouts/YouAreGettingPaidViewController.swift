db41a7fbc91c1ab16c84d4c6482bc34d9604260f
Michał Buczek <michal.buczek@protonmail.com>
2020-01-16 18:12:00 +0100

    add link support.

--- Client/Frontend/NTP/BrandedImageCallouts/YouAreGettingPaidViewController.swift
+++ Client/Frontend/NTP/BrandedImageCallouts/YouAreGettingPaidViewController.swift
@@ -5,6 +5,7 @@
 import UIKit
 import Shared
 import BraveShared
+import BraveRewardsUI
 
 extension BrandedImageCallout {
     class YouAreGettingPaidViewController: UIViewController {
@@ -20,26 +21,27 @@ extension BrandedImageCallout {
         let body = UILabel().then {
             $0.text = "You're getting paid to see this background image."
             $0.appearanceTextColor = .black
-            
+
             $0.font = UIFont.systemFont(ofSize: 14, weight: .medium)
-            
+
             $0.numberOfLines = 0
             $0.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
             $0.lineBreakMode = .byWordWrapping
         }
         
-        let body2 = UILabel().then {
+        let body2 = LinkLabel().then {
+            $0.font = .systemFont(ofSize: 14.0)
+            $0.appearanceTextColor = .black
+            $0.linkColor = BraveUX.braveOrange
             $0.text = """
             Learn more about sponsored images in Brave Rewards. You can also choose \
             to hide sponsored images.
             """
-            $0.appearanceTextColor = .black
+            $0.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+            $0.setURLInfo(["Learn more": "terms", "hide sponsored images": "policy"])
             
-            $0.font = UIFont.systemFont(ofSize: 12, weight: .regular)
-            
-            $0.numberOfLines = 0
-            $0.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 313), for: .vertical)
-            $0.lineBreakMode = .byWordWrapping
+            $0.textContainerInset = UIEdgeInsets.zero
+            $0.textContainer.lineFragmentPadding = 0
         }
         
         override func viewDidLoad() {
@@ -57,12 +59,16 @@ extension BrandedImageCallout {
                 $0.left.right.equalToSuperview().inset(16)
                 $0.bottom.equalToSuperview().inset(48)
             }
+            
+            let size = body.sizeThatFits(CGSize(width: view.frame.width - 32, height: CGFloat.infinity))
+            
+            body.snp.remakeConstraints {
+                $0.height.equalTo(size.height)
+            }
         }
         
         override func viewDidLayoutSubviews() {
-            [body, body2].forEach {
-                $0.preferredMaxLayoutWidth = view.frame.width - 32
-            }
+            body.preferredMaxLayoutWidth = view.frame.width - 32
         }
     }
 }
