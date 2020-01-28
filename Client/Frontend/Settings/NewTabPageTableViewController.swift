78e7495db19518a459ca039f872a01b1b4991873
Joel Reis <joelhreis@gmail.com>
2020-01-28 05:59:17 -0500

    Fix #2151, Fix #2083: Branded background images (#2203)

--- Client/Frontend/Settings/NewTabPageTableViewController.swift
+++ Client/Frontend/Settings/NewTabPageTableViewController.swift
@@ -6,6 +6,7 @@ import Foundation
 import Static
 import BraveShared
 import Shared
+import BraveRewards
 
 class NewTabPageTableViewController: TableViewController {
     let sponsoredRow = BoolRow(title: Strings.NewTabPageSettingsSponsoredImages, option: Preferences.NewTabPage.backgroundSponsoredImages)
@@ -34,22 +35,19 @@ class NewTabPageTableViewController: TableViewController {
                 // If turning off normal background images, turn of sponsored images as well.
                 Preferences.NewTabPage.backgroundSponsoredImages.value = newValue
                 
-                if !newValue {
-                    // Updating the underlying preference does not dynamically update the visuals unfortuantely.
-                    // Updating manually. Only update if disabling.
-                    self.sponsoredSwitch?.isOn = newValue
-                }
+                // Update sponsored switch to both on/off to follow background images.
+                self.sponsoredSwitch?.isOn = newValue
                 
                 // Need to update every time.
                 self.sponsoredSwitch?.isEnabled = newValue
             })
         ]
         
-        if BackgroundImage.showSponsoredSetting {
+        if BraveAds.isCurrentLocaleSupported() {
             rows.append(sponsoredRow)
         }
-        
-        rows.append(BoolRow(title: Strings.NewTabPageSettingsAutoOpenKeyboard, option: Preferences.NewTabPage.autoOpenKeyboard))
+        rows.append(BoolRow(title: Strings.NewTabPageSettingsAutoOpenKeyboard,
+                             option: Preferences.NewTabPage.autoOpenKeyboard))
         return Section(rows: rows)
     }()
     
