5d7c2c0093d2f6c05af364ff05cf38071367c075
Soner YUKSEL <grkmyksl@gmail.com>
2020-11-23 13:47:15 -0500

    Fix#3059: Browser crashes when closing Tab from TabTray (#3066)

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1615,8 +1615,11 @@ class BrowserViewController: UIViewController {
         self.tabTrayController = tabTrayController
     }
 
-    func switchToTabForURLOrOpen(_ url: URL, isPrivate: Bool = false, isPrivileged: Bool) {
-        popToBVC()
+    func switchToTabForURLOrOpen(_ url: URL, isPrivate: Bool = false, isPrivileged: Bool, isExternal: Bool = false) {
+        if !isExternal {
+            popToBVC()
+        }
+        
         if let tab = tabManager.getTabForURL(url) {
             tabManager.selectTab(tab)
         } else {
@@ -1660,11 +1663,12 @@ class BrowserViewController: UIViewController {
         }
     }
 
-    fileprivate func popToBVC() {
+    func popToBVC() {
         guard let currentViewController = navigationController?.topViewController else {
-                return
+            return
         }
         currentViewController.dismiss(animated: true, completion: nil)
+        
         if currentViewController != self {
             _ = self.navigationController?.popViewController(animated: true)
         } else if topToolbar.inOverlayMode {
