1b4cda3131f67efb4543db68f82283192272919b
Soner Yuksel <grkmyksl@gmail.com>
2021-01-26 16:51:13 -0500

    Adding cache for images in the thumbnail, response, memory, disk

--- Client/Frontend/Browser/Playlist/PlaylistMediaInfo.swift
+++ Client/Frontend/Browser/Playlist/PlaylistMediaInfo.swift
@@ -415,24 +415,3 @@ extension MediaResourceManager {
         }.resume()
     }
 }
-
-extension AVAsset {
-    func generateThumbnail(completion: @escaping (UIImage?) -> Void) {
-        DispatchQueue.global().async {
-            let imageGenerator = AVAssetImageGenerator(asset: self)
-            let time = CMTime(seconds: 0.0, preferredTimescale: 600)
-            let times = [NSValue(time: time)]
-            imageGenerator.generateCGImagesAsynchronously(forTimes: times, completionHandler: { _, image, _, _, _ in
-                if let image = image {
-                    DispatchQueue.main.async {
-                        completion(UIImage(cgImage: image))
-                    }
-                } else {
-                    DispatchQueue.main.async {
-                        completion(nil)
-                    }
-                }
-            })
-        }
-    }
-}
