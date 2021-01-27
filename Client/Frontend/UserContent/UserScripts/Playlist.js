499a0512998e980f275cc85a1b7422e89dd95885
Brandon <JustBrandonT@gmail.com>
2021-01-27 09:15:16 -0500

    Fixing live streaming videos

--- Client/Frontend/UserContent/UserScripts/Playlist.js
+++ Client/Frontend/UserContent/UserScripts/Playlist.js
@@ -7,15 +7,42 @@ window.__firefox__.includeOnce("Playlist", function() {
             if (name == null || typeof name == 'undefined' || name == "") {
                 name = document.title;
             }
-        
-            window.webkit.messageHandlers.playlistHelper.postMessage({
-                                                                        "name": name,
-                                                                        "src": target.src,
-                                                                        "pageSrc": window.location.href,
-                                                                        "pageTitle": document.title,
-                                                                        "mimeType": type,
-                                                                        "duration": target.duration !== target.duration ? 0.0 : target.duration
-                                                                        });
+            
+            if (target.src != "") {
+                window.webkit.messageHandlers.playlistHelper.postMessage({
+                                                                            "name": name,
+                                                                            "src": target.src,
+                                                                            "pageSrc": window.location.href,
+                                                                            "pageTitle": document.title,
+                                                                            "mimeType": type,
+                                                                            "duration": target.duration !== target.duration ? 0.0 : target.duration
+                                                                            });
+            }
+            else {
+                document.querySelectorAll('source').forEach(function(node) {
+                    if (node.closest('video') === target) {
+                        window.webkit.messageHandlers.playlistHelper.postMessage({
+                                                                                    "name": name,
+                                                                                    "src": target.src,
+                                                                                    "pageSrc": window.location.href,
+                                                                                    "pageTitle": document.title,
+                                                                                    "mimeType": type,
+                                                                                    "duration": target.duration !== target.duration ? 0.0 : target.duration
+                                                                                    });
+                    }
+                    
+                    if (node.closest('audio') === target) {
+                        window.webkit.messageHandlers.playlistHelper.postMessage({
+                                                                                    "name": name,
+                                                                                    "src": target.src,
+                                                                                    "pageSrc": window.location.href,
+                                                                                    "pageTitle": document.title,
+                                                                                    "mimeType": type,
+                                                                                    "duration": target.duration !== target.duration ? 0.0 : target.duration
+                                                                                    });
+                    }
+                });
+            }
         }
     }
     
@@ -119,124 +146,11 @@ window.__firefox__.includeOnce("Playlist", function() {
     }
     
     setupLongPress();
-    
-//    window.addEventListener("touchstart", function(event) {
-//
-//    }, true);
 });
 
 
-//function notifyNodeSource(node, src, mimeType) {
-//    var name = node.title;
-//    if (name == null || typeof name == 'undefined' || name == "") {
-//        name = document.title;
-//    }
-//    
-//    if (mimeType == null || typeof mimeType == 'undefined' || mimeType == "") {
-//        if (node.constructor.name == 'HTMLVideoElement') {
-//            mimeType = 'video';
-//        }
-//        
-//        if (node.constructor.name == 'HTMLAudioElement') {
-//            mimeType = 'audio';
-//        }
-//    }
-//    
-//    if (src != "") {
-//        window.webkit.messageHandlers.playlistHelper.postMessage({
-//                                                                    "name": name,
-//                                                                    "src": src,
-//                                                                    "pageSrc": window.location.href,
-//                                                                    "pageTitle": document.title,
-//                                                                    "mimeType": mimeType,
-//                                                                    "duration": node.duration !== node.duration ? 0.0 : node.duration
-//                                                                    });
-//    }
-//}
-//
-//function notifyNode(node) {
-//    notifyNodeSource(node, node.src, node.type);
-//}
-//
-//function observeNode(node) {
-//    if (node.observer == null || node.observer === undefined) {
-//        node.observer = new MutationObserver(function (mutations) {
-//            notifyNode(node);
-//            
-////            mutations.forEach((mutation) => {
-////                if (mutation.type === 'childList') {
-////                    mutation.addedNodes.forEach((child) => {
-////                        if (child.constructor.name === 'HTMLSourceElement') {
-////                            notifyNodeSource(node, child.src, child.type);
-////                        }
-////                    });
-////                }
-////            });
-//        });
-//        node.observer.observe(node, { attributes: true, attributeFilter: ["src"] });
-//        //node.observer.observe(node, { childList: true });
-//        notifyNode(node);
-//        
-//        node.addEventListener('loadedmetadata', function() {
-//            notifyNode(node);
-//        });
-//    }
-//}
-//
-//function observeDocument(node) {
-//    if (node.observer == null || node.observer === undefined) {
-//        node.observer = new MutationObserver(function (mutations) {
-//            mutations.forEach(function (mutation) {
-//                mutation.addedNodes.forEach(function (node) {
-//                    if (node.constructor.name == "HTMLVideoElement") {
-//                        observeNode(node);
-//                    }
-//                    else if (node.constructor.name == "HTMLAudioElement") {
-//                        observeNode(node);
-//                    }
-////                    else if (node.constructor.name == "HTMLSourceElement") {
-////                        if (node.parentNode.constructor.name == "HTMLVideoElement") {
-////                            console.log('Found Child');
-////                        }
-////                    }
-//                });
-//            });
-//        });
-//        node.observer.observe(node, { subtree: true, childList: true });
-//    }
-//}
-//
-//function observeDynamicElements(node) {
-//    var original = node.createElement;
-//    node.createElement = function (tag) {
-//        if (tag === 'audio' || tag === 'video') {
-//            var result = original.call(node, tag);
-//            observeNode(result);
-//            notifyNode(result);
-//            return result;
-//        }
-//        return original.call(node, tag);
-//    };
-//}
-//
-//function getAllVideoElements() {
-//    return document.querySelectorAll('video');
-//}
-//
-//function getAllAudioElements() {
-//    return document.querySelectorAll('audio');
-//}
-//
-//function onReady(fn) {
-//    if (document.readyState === "complete" || document.readyState === "interactive") {
-//        setTimeout(fn, 1);
-//    } else {
-//        document.addEventListener("DOMContentLoaded", fn);
-//    }
-//}
-//
 //function setZoom(zoom,el) {
-//      
+//
 //      transformOrigin = [0,0];
 //        el = el || instance.getContainer();
 //        var p = ["webkit", "moz", "ms", "o"],
@@ -250,7 +164,7 @@ window.__firefox__.includeOnce("Playlist", function() {
 //
 //        el.style["transform"] = s;
 //        el.style["transformOrigin"] = oString;
-//      
+//
 //}
 //
 //var zoom = 1;
@@ -270,38 +184,3 @@ window.__firefox__.includeOnce("Playlist", function() {
 //    document.body.style.transform = "scale(" + zoom + ")";
 //    document.body.style.width = width + "%";
 //}
-//
-////TODO: Modify to not use mutation observers
-////TODO: Modify to not use intervals
-////^ Fix all of the above using a node.add and node.insert hook instead.
-//function observePage() {
-//    observeDocument(document);
-//    observeDynamicElements(document);
-//    
-////    onReady(function() {
-////        getAllVideoElements().forEach(function(node) {
-////            observeNode(node);
-////            notifyNode(node);
-////        });
-////    });
-//    
-//    // Timeinterval is needed for DailyMotion as their DOM is bad
-//    var interval = setInterval(function(){
-//        getAllVideoElements().forEach(function(node) {
-//            observeNode(node);
-//            notifyNode(node);
-//        });
-//        
-//        getAllAudioElements().forEach(function(node) {
-//            observeNode(node);
-//            notifyNode(node);
-//        });
-//    }, 1000);
-//    
-//    var timeout = setTimeout(function() {
-//        clearInterval(interval);
-//        clearTimeout(timeout);
-//    }, 10000);
-//}
-//
-//observePage();
