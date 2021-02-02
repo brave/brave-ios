d774a07f3ce30ad1441286bb60efb5fe3ad152bc
Brandon <JustBrandonT@gmail.com>
2021-02-01 21:02:16 -0500

    Fixing playback of cached videos. Apple's API does NOT download MP4's or container types. It only handles HLS streams, so we download all other container types manually.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -2662,7 +2662,7 @@ extension BrowserViewController: TabDelegate {
         
         let playlistHelper = PlaylistHelper(tab: tab)
         playlistHelper.delegate = self
-        tab.addContentScript(playlistHelper, name: PlaylistHelper.name())
+        tab.addContentScript(playlistHelper, name: PlaylistHelper.name(), sandboxed: false)
 
         tab.addContentScript(RewardsReporting(rewards: rewards, tab: tab), name: RewardsReporting.name())
         tab.addContentScript(AdsMediaReporting(rewards: rewards, tab: tab), name: AdsMediaReporting.name())
