brave/brave-core

commit f873a329432442caeb48e64738f0ef8536e9cb72

Fix #2644, Fix #2558: Tabs and edit bookmarks alert respect dark mode (#3029)

Client/Frontend/Browser/TabsBar/TabBarCell.swift
@@ -5,7 +5,7 @@
 import UIKit
 import BraveShared
 
-class TabBarCell: UICollectionViewCell {
+class TabBarCell: UICollectionViewCell, Themeable {
     
     lazy var titleLabel: UILabel = {
         let label = UILabel()
@@ -17,9 +17,11 @@ class TabBarCell: UICollectionViewCell {
         let button = UIButton()
         button.addTarget(self, action: #selector(closeTab), for: .touchUpInside)
         button.setImage(#imageLiteral(resourceName: "close_tab_bar").template, for: .normal)
-        button.tintColor = PrivateBrowsingManager.shared.isPrivateBrowsing ? UIColor.white : UIColor.black
+        button.tintColor = PrivateBrowsingManager.shared.isPrivateBrowsing ? .white : .black
+        
         // Close button is a bit wider to increase tap area, this aligns the 'X' image closer to the right.
         button.imageEdgeInsets.left = 6
+        
         return button
     }()
     
@@ -115,6 +117,7 @@ class TabBarCell: UICollectionViewCell {
     }
     
     fileprivate var titleUpdateScheduled = false
+    
     func updateTitleThrottled(for tab: Tab) {
         if titleUpdateScheduled {
             return
@@ -125,5 +128,11 @@ class TabBarCell: UICollectionViewCell {
             strongSelf.titleUpdateScheduled = false
             strongSelf.titleLabel.text = tab.displayTitle
         }
+    }
+    
+    func applyTheme(_ theme: Theme) {        
+        backgroundColor = .clear
+        titleLabel.textColor = theme.colors.tints.header
+        closeButton.tintColor = theme.colors.tints.header
     }
 }

Client/Extensions/UIAlertControllerExtensions.swift
@@ -242,9 +242,17 @@ class UserTextInputAlert {
     private func textFieldConfig(text: String?, placeholder: String?, keyboardType: UIKeyboardType?, forcedInput: Bool)
         -> (UITextField) -> Void {
             return { textField in
-                textField.placeholder = placeholder
+                if #available(iOS 13.0, *) {
+                    textField.attributedPlaceholder = NSAttributedString(
+                        string: placeholder ?? "",
+                        attributes: [.foregroundColor: UIColor.placeholderText])
+                } else {
+                    textField.placeholder = placeholder
+                }
+                
+                textField.keyboardAppearance = .default
+
                 textField.isSecureTextEntry = false
-                textField.keyboardAppearance = .dark
                 textField.autocapitalizationType = keyboardType == .URL ? .none : .words
                 textField.autocorrectionType = keyboardType == .URL ? .no : .default
                 textField.returnKeyType = .done

Client/Frontend/Browser/TabsBar/TabsBarViewController.swift
@@ -14,6 +14,11 @@ protocol TabsBarViewControllerDelegate: class {
 }
 
 class TabsBarViewController: UIViewController {
+    
+    private struct ReuseIdentifier {
+        static let tabBarCell = "TabBarCell"
+    }
+    
     private let leftOverflowIndicator = CAGradientLayer()
     private let rightOverflowIndicator = CAGradientLayer()
     
@@ -44,7 +49,7 @@ class TabsBarViewController: UIViewController {
         view.dataSource = self
         view.allowsSelection = true
         view.decelerationRate = UIScrollView.DecelerationRate.normal
-        view.register(TabBarCell.self, forCellWithReuseIdentifier: "TabCell")
+        view.register(TabBarCell.self, forCellWithReuseIdentifier: ReuseIdentifier.tabBarCell)
         return view
     }()
     
@@ -52,9 +57,10 @@ class TabsBarViewController: UIViewController {
     
     fileprivate weak var tabManager: TabManager?
     fileprivate var tabList = WeakList<Tab>()
-    
+        
     init(tabManager: TabManager) {
         self.tabManager = tabManager
+        
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -104,12 +110,31 @@ class TabsBarViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         // Updating tabs here is especially handy when tabs are reordered from the tab tray.
         updateData()
+        
+        if #available(iOS 13.0, *) {
+            let oldTraitCollection = UITraitCollection.current
+            UITraitCollection.current = traitCollection
+            applyTheme(Theme.of(tabManager?.selectedTab))
+            UITraitCollection.current = oldTraitCollection
+        } else {
+            applyTheme(Theme.of(tabManager?.selectedTab))
+        }
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         updateOverflowIndicatorsLayout()
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        if #available(iOS 13.0, *) {
+            if traitCollection.userInterfaceStyle != previousTraitCollection?.userInterfaceStyle {
+                applyTheme(Theme.of(tabManager?.selectedTab))
+            }
+        }
     }
     
     deinit {
@@ -279,8 +304,10 @@ extension TabsBarViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TabCell", for: indexPath) as? TabBarCell
-            else { return UICollectionViewCell() }
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReuseIdentifier.tabBarCell, for: indexPath) as? TabBarCell else {
+            return UICollectionViewCell()
+        }
+        
         guard let tab = tabList[indexPath.row] else { return cell }
         
         cell.tabManager = tabManager
@@ -288,17 +315,20 @@ extension TabsBarViewController: UICollectionViewDataSource {
         cell.titleLabel.text = tab.displayTitle
         cell.currentIndex = indexPath.row
         cell.separatorLineRight.isHidden = (indexPath.row != tabList.count() - 1)
-        
+        cell.applyTheme(Theme.of(tab))
+
         cell.closeTabCallback = { [weak self] tab in
-            guard let strongSelf = self, let tabManager = strongSelf.tabManager, let previousIndex = strongSelf.tabList.index(of: tab) else { return }
+            guard let self = self,
+                  let tabManager = self.tabManager,
+                  let previousIndex = self.tabList.index(of: tab) else { return }
             
             tabManager.removeTab(tab)
-            strongSelf.updateData()
+            self.updateData()
             
             let previousOrNext = max(0, previousIndex - 1)
-            tabManager.selectTab(strongSelf.tabList[previousOrNext])
+            tabManager.selectTab(self.tabList[previousOrNext])
             
-            strongSelf.collectionView.selectItem(at: IndexPath(row: previousOrNext, section: 0), animated: true, scrollPosition: .centeredHorizontally)
+            self.collectionView.selectItem(at: IndexPath(row: previousOrNext, section: 0), animated: true, scrollPosition: .centeredHorizontally)
         }
         
         return cell
@@ -343,6 +373,10 @@ extension TabsBarViewController: TabManagerDelegate {
 }
 
 extension TabsBarViewController: Themeable {
+    var themeableChildren: [Themeable?]? {
+        collectionView.visibleCells.compactMap({ $0 as? TabBarCell })
+    }
+    
     func applyTheme(_ theme: Theme) {
         styleChildren(theme: theme)
         

brave/brave-ios

commit 448e79b854836c959caf0b0cb4065fab02325e06

Fix #3253: Better distinction between active and inactive tabs.
Regression caused by PR #3029.

Client/Frontend/Browser/TabsBar/TabBarCell.swift
@@ -131,7 +131,6 @@ class TabBarCell: UICollectionViewCell, Themeable {
     }
     
     func applyTheme(_ theme: Theme) {        
-        backgroundColor = .clear
         titleLabel.textColor = theme.colors.tints.header
         closeButton.tintColor = theme.colors.tints.header
     }