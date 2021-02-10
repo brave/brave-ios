5bf57c760cedee1bcd83871a74be5703b760177e
Michał Buczek <michal.buczek@protonmail.com>
2021-02-10 18:24:57 +0100

    Fix #3287: Do not show default browser callout on iOS13 and lower.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -954,7 +954,9 @@ class BrowserViewController: UIViewController {
             self.presentVPNCallout()
         }
         
-        presentDefaultBrowserIntroScreen()
+        if #available(*, iOS 14) {
+            presentDefaultBrowserIntroScreen()
+        }
         
         screenshotHelper.viewIsVisible = true
         screenshotHelper.takePendingScreenshots(tabManager.allTabs)
