aba8878cef9c1b5be662cb586139d1c207de38ea
Brandon <JustBrandonT@gmail.com>
2021-04-08 14:11:34 -0400

    Set the default value of playlists to auto-download: on.

--- Client/Application/ClientPreferences.swift
+++ Client/Application/ClientPreferences.swift
@@ -212,7 +212,7 @@ extension Preferences {
         /// Whether to play the video when controller loaded
         static let firstLoadAutoPlay = Option<Bool>(key: "playlist.firstLoadAutoPlay", default: false)
         /// The Option to download video yes / no / only wifi
-        static let autoDownloadVideo = Option<String>(key: "playlist.listViewSide", default: PlayListDownloadType.off.rawValue)
+        static let autoDownloadVideo = Option<String>(key: "playlist.autoDownload", default: PlayListDownloadType.on.rawValue)
         /// The Option to disable playlist MediaSource web-compatibility
         static let webMediaSourceCompatibility = Option<Bool>(key: "playlist.webMediaSourceCompatibility", default: UIDevice.isIpad)
         /// The option to start the playback where user left-off
