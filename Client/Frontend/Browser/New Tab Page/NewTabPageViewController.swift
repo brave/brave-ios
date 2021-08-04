53677c74b5364a7a24e4b01f97870675693bd495
Kyle Hickinson <git@kylehickinson.com>
2021-08-04 17:03:59 -0400

    Fix Ads enum name change

--- Client/Frontend/Browser/New Tab Page/NewTabPageViewController.swift
+++ Client/Frontend/Browser/New Tab Page/NewTabPageViewController.swift
@@ -377,7 +377,7 @@ class NewTabPageViewController: UIViewController {
         backgroundView.imageConstraints?.landscapeCenter.update(offset: inset)
     }
     
-    private func reportSponsoredImageBackgroundEvent(_ event: NewTabPageAdEventType) {
+    private func reportSponsoredImageBackgroundEvent(_ event: BraveAdsNewTabPageAdEventType) {
         guard let backgroundType = background.currentBackground?.type,
               case .withBrandLogo = backgroundType,
               let creativeInstanceId = background.currentBackground?.wallpaper.creativeInstanceId else {
