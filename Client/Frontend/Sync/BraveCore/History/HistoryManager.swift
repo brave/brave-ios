de130df008dc34d44e53646c703e9b8144fb81e6
Soner Yuksel <grkmyksl@gmail.com>
2021-08-05 16:15:50 -0400

    History Manager methods and constructor in AppDelegate
    (cherry picked from commit 473045639547af7c6542d5e4aab7c4aa647aafb0)
    (cherry picked from commit 16d9183020a565d8be8d7ef141397552f9b8de18)

---
+++ Client/Frontend/Sync/BraveCore/History/HistoryManager.swift
@@ -0,0 +1,115 @@
+// Copyright 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Foundation
+import Data
+import BraveCore
+import BraveShared
+import CoreData
+import Shared
+
+class HistoryManager: NSObject {
+    
+    private var observer: HistoryServiceListener?
+    private let historyAPI: BraveHistoryAPI?
+    
+    init(historyAPI: BraveHistoryAPI?) {
+        self.historyAPI = historyAPI
+        
+        super.init()
+    }
+    
+    public func add(url: URL, title: String, dateAdded: Date, isURLTyped: Bool = true) {
+        guard let historyAPI = historyAPI else {
+            return
+        }
+
+        let historyNode = HistoryNode(url: url, title: title, dateAdded: dateAdded)
+        historyAPI.addHistory(historyNode, isURLTyped: isURLTyped)
+    }
+
+    public func frc() -> HistoryV2FetchResultsController? {
+        guard let historyAPI = self.historyAPI else {
+            return nil
+        }
+
+        return Historyv2Fetcher(historyAPI: historyAPI)
+    }
+
+    public func delete(_ historyNode: HistoryNode) {
+        guard let historyAPI = self.historyAPI else {
+            return
+        }
+
+        historyAPI.removeHistory(historyNode)
+    }
+
+    public func deleteAll(_ completion: @escaping () -> Void) {
+        guard let historyAPI = self.historyAPI else {
+            return
+        }
+
+        historyAPI.removeAll {
+            completion()
+        }
+    }
+
+    public func suffix(_ maxLength: Int, _ completion: @escaping ([Historyv2]) -> Void) {
+        guard let historyAPI = self.historyAPI else {
+            return
+        }
+
+        historyAPI.search(withQuery: nil, maxCount: UInt(max(20, maxLength)), completion: { historyResults in
+            completion(historyResults.map { Historyv2(with: $0) })
+        })
+    }
+
+    public func byFrequency(query: String? = nil, _ completion: @escaping ([WebsitePresentable]) -> Void) {
+        guard let query = query, !query.isEmpty,
+              let historyAPI = self.historyAPI else {
+            return
+        }
+
+        historyAPI.search(withQuery: query, maxCount: 200, completion: { historyResults in
+            completion(historyResults.map { Historyv2(with: $0) })
+        })
+    }
+
+    public func update(_ historyNode: HistoryNode, customTitle: String?, dateAdded: Date?) {
+        if let title = customTitle {
+            historyNode.title = title
+        }
+
+        if let date = dateAdded {
+            historyNode.dateAdded = date
+        }
+    }
+}
+
+// MARK: Brave-Core Only
+
+extension HistoryManager {
+    
+    public func waitForHistoryServiceLoaded(_ completion: @escaping () -> Void) {
+        guard let historyAPI = self.historyAPI else { return }
+        
+        if historyAPI.isBackendLoaded {
+            DispatchQueue.main.async {
+                completion()
+            }
+        } else {
+            observer = historyAPI.add(HistoryServiceStateObserver({ [weak self] in
+                if case .serviceLoaded = $0 {
+                    self?.observer?.destroy()
+                    self?.observer = nil
+                    
+                    DispatchQueue.main.async {
+                        completion()
+                    }
+                }
+            }))
+        }
+    }
+}
