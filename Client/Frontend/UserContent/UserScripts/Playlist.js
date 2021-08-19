912afe8726aff9f2a77317c061d994bee5ef3fc0
Brandon T <JustBrandonT@gmail.com>
2021-08-19 14:53:11 -0400

    Removed `cryptographically` because security doesn't endorse it.

--- Client/Frontend/UserContent/UserScripts/Playlist.js
+++ Client/Frontend/UserContent/UserScripts/Playlist.js
@@ -40,7 +40,6 @@ window.__firefox__.includeOnce("$<Playlist>", function() {
     }
     
     function uuid_v4() {
-        // JS cryptographically secure UUIDv4.
         return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
         );
