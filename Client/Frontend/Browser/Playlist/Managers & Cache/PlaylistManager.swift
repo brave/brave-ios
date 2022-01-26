33c06b013b7ff5cb61b0686c7107948d79873e6b
Brandon T <JustBrandonT@gmail.com>
2022-01-26 13:58:50 -0500

    Fixing PIP Added PlaylistFolder UI Fix Thumbnail Utility
    Fix CoreData model for folders crash.
    Add Drag-Drop
    Add creating folders
    Add listeners for folder events.
    
    Fix some folder issues
    Add moving Playlist items.
    Use a migration instead of nil folder
    Fix CarPlay
    Sync CarPlay and iPhone UI together.
    
    Fixed items not showing as they're added to a nil folder.
    Fixed deleting items.
    
    Fixing lots of bugs.
    Fix iPad not playing.
    Fix iPad bugs.
    Removed unneeded alignments and dimensions.
    Fix Move Folders aspect ratios, sizes, and landscape layout.
    
    Move code to the section header so it doesn't get clipped.
    Pause playing when opening the move screen.
    Fix drag-drop previews.
    Added context-menu.
    
    Localizing everything and addressing feedback.
    
    Fix unit test.
    Fixed strings and edit folder screen text.
    Added Swipe to Edit.
    Fixed item sizes
    Fixed tableView background and moveItems
    Remove transition styles
    Use built-in toolbar.
    Fix adaptive layout to not restrict to 3 items max.
    Fixed flickering
    Resolved ALL feedback.

--- Client/Frontend/Browser/Playlist/Managers & Cache/PlaylistManager.swift
+++ Client/Frontend/Browser/Playlist/Managers & Cache/PlaylistManager.swift
@@ -14,21 +14,12 @@ import BraveShared
 
 private let log = Logger.browserLogger
 
-protocol PlaylistManagerDelegate: AnyObject {
-    func onDownloadProgressUpdate(id: String, percentComplete: Double)
-    func onDownloadStateChanged(id: String, state: PlaylistDownloadManager.DownloadState, displayName: String?, error: Error?)
-    
-    func controllerDidChange(_ anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?)
-    func controllerDidChangeContent()
-    func controllerWillChangeContent()
-}
-
 class PlaylistManager: NSObject {
     static let shared = PlaylistManager()
     
     private var assetInformation = [PlaylistAssetFetcher]()
     private let downloadManager = PlaylistDownloadManager()
-    private let frc = PlaylistItem.frc()
+    private var frc = PlaylistItem.frc()
     private var didRestoreSession = false
     
     // Observers
@@ -45,6 +36,7 @@ class PlaylistManager: NSObject {
                                                              state: PlaylistDownloadManager.DownloadState,
                                                              displayName: String?,
                                                              error: Error?), Never>()
+    private let onCurrentFolderChanged = PassthroughSubject<(), Never>()
     
     private override init() {
         super.init()
@@ -54,6 +46,25 @@ class PlaylistManager: NSObject {
         
         // Delete system cache always on startup.
         deleteUserManagedAssets()
+    }
+    
+    var currentFolder: PlaylistFolder? {
+        didSet {
+            frc.delegate = nil
+            
+            if let currentFolder = currentFolder {
+                // Only return an FRC for the specified folder
+                frc = PlaylistItem.frc(parentFolder: currentFolder)
+            } else {
+                // Return every folder, including the "Saved" folder
+                frc = PlaylistItem.allFoldersFRC()
+            }
+            
+            frc.delegate = self
+            reloadData()
+            
+            onCurrentFolderChanged.send()
+        }
     }
     
     var contentWillChange: AnyPublisher<Void, Never> {
@@ -76,12 +87,20 @@ class PlaylistManager: NSObject {
         onDownloadStateChanged.eraseToAnyPublisher()
     }
     
+    var onCurrentFolderDidChange: AnyPublisher<(), Never> {
+        onCurrentFolderChanged.eraseToAnyPublisher()
+    }
+    
     var allItems: [PlaylistInfo] {
         frc.fetchedObjects?.map({ PlaylistInfo(item: $0) }) ?? []
     }
     
     var numberOfAssets: Int {
         frc.fetchedObjects?.count ?? 0
+    }
+    
+    var fetchedObjects: [PlaylistItem] {
+        frc.fetchedObjects ?? []
     }
     
     func itemAtIndex(_ index: Int) -> PlaylistInfo? {
@@ -222,6 +241,24 @@ class PlaylistManager: NSObject {
     
     func cancelDownload(item: PlaylistInfo) {
         downloadManager.cancelDownload(item: item)
+    }
+    
+    func delete(folder: PlaylistFolder) {
+        if currentFolder?.objectID == folder.objectID {
+            currentFolder = nil
+        }
+        
+        folder.playlistItems?.forEach({
+            self.delete(item: PlaylistInfo(item: $0))
+        })
+        
+        if folder.uuid != PlaylistFolder.savedFolderUUID {
+            PlaylistFolder.removeFolder(folder)
+        }
+        
+        if currentFolder?.isDeleted == true {
+            currentFolder = nil
+        }
     }
     
     @discardableResult
