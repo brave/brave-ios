217565388fa98b4cd67915b896f851a6e66a9991
Kyle Hickinson <git@kylehickinson.com>
2022-02-15 14:39:48 -0500

    Fix iOS 14 missing padding on add/edit screen

--- BraveWallet/Settings/CustomNetworkDetailsView.swift
+++ BraveWallet/Settings/CustomNetworkDetailsView.swift
@@ -254,6 +254,13 @@ struct CustomNetworkDetailsView: View {
     Form {
       Section(
         header: WalletListHeaderView(title: Text(Strings.Wallet.customNetworkChainIdTitle))
+          .osAvailabilityModifiers { content in
+            if #available(iOS 15.0, *) {
+              content // padding already exists in 15
+            } else {
+              content.padding(.top)
+            }
+          }
       ) {
         NetworkTextField(
           placeholder: Strings.Wallet.customNetworkChainIdPlaceholder,
