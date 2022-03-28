2fb2dffcdbdadd5a73fc4127f3bc73469f702268
Jacob Sikorski <jacob.sikorski@gmail.com>
2022-03-28 13:18:31 -0400

    Fix #3700: Minor cleanup of tests

--- ClientTests/User Scripts/ScriptFactoryTests.swift
+++ ClientTests/User Scripts/ScriptFactoryTests.swift
@@ -10,25 +10,26 @@ class ScriptFactoryTests: XCTestCase {
   private let scriptFactory = ScriptFactory()
 
   func testLoadingFarblingProtectionScript() throws {
+    // Given
+    // Different ETLD+1s
     let etld = URL(string: "https://example.com")!.baseDomain!
     let etld2 = URL(string: "https://brave.com")!.baseDomain!
 
-    do {
-      let script1 = try scriptFactory.makeScript(for: .farblingProtection(etld: etld))
-      let script2 = try scriptFactory.makeScript(for: .farblingProtection(etld: etld))
-      let script3 = try scriptFactory.makeScript(for: .farblingProtection(etld: etld2))
+    // When
+    // Making scripts from the different ETLD+1s
+    let script1 = try scriptFactory.makeScript(for: .farblingProtection(etld: etld))
+    let script2 = try scriptFactory.makeScript(for: .farblingProtection(etld: etld))
+    let script3 = try scriptFactory.makeScript(for: .farblingProtection(etld: etld2))
 
-      // Test cached value is returned (i.e. Same script pointer)
-      XCTAssertEqual(script1, script2)
-      // Test the script is not modfied differently (i.e. same seed is injected)
-      XCTAssertEqual(script1.source, script2.source)
-      // Test no existing cache is not returned (i.e. Different script pointer)
-      XCTAssertNotEqual(script1, script3)
-      // Test the script is modified differently (i.e. different seed is injected)
-      XCTAssertNotEqual(script1.source, script3.source)
-    } catch {
-      XCTAssertNoThrow({ throw error })
-    }
+    // Then
+    // Test cached value is returned (i.e. Same script pointer)
+    XCTAssertEqual(script1, script2)
+    // Test the script is not modfied differently (i.e. same seed is injected)
+    XCTAssertEqual(script1.source, script2.source)
+    // Test no existing cache is not returned (i.e. Different script pointer)
+    XCTAssertNotEqual(script1, script3)
+    // Test the script is modified differently (i.e. different seed is injected)
+    XCTAssertNotEqual(script1.source, script3.source)
   }
 
   func testLoadingScriptDomainScripts() throws {
