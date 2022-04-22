c0814fe79f607aa3584149a6fb596921291a7cb6
Michał Buczek <michal.buczek@protonmail.com>
2022-04-22 15:56:28 +0200

    Remove indentation spacing from selected files

--- Client/Frontend/Browser/BrowserViewController/BrowserViewController+Onboarding.swift
+++ Client/Frontend/Browser/BrowserViewController/BrowserViewController+Onboarding.swift
@@ -11,22 +11,22 @@ import BraveCore
 // MARK: - Onboarding
 
 extension BrowserViewController {
-  
+
   func presentOnboardingIntro() {
     if Preferences.DebugFlag.skipOnboardingIntro == true { return }
-    
+
     presentOnboardingWelcomeScreen(on: self)
   }
-  
+
   func presentOnboardingWelcomeScreen(on parentController: UIViewController) {
     if Preferences.DebugFlag.skipOnboardingIntro == true { return }
-    
+
     // 1. Existing user.
     // 2. User already completed onboarding.
     if Preferences.General.basicOnboardingCompleted.value == OnboardingState.completed.rawValue {
       return
     }
-    
+
     // 1. User is brand new
     // 2. User hasn't completed onboarding
     if Preferences.General.basicOnboardingCompleted.value != OnboardingState.completed.rawValue,
@@ -37,11 +37,11 @@ extension BrowserViewController {
       onboardingController.modalPresentationStyle = .fullScreen
       onboardingController.onAdsWebsiteSelected = { [weak self] url in
         guard let self = self else { return }
-        
+
         if let url = url {
           self.topToolbar.leaveOverlayMode()
           self.addNTPTutorialPage()
-          
+
           let tab = self.tabManager.addTab(
             PrivilegedRequest(url: url) as URLRequest,
             afterTab: self.tabManager.selectedTab,
@@ -59,12 +59,12 @@ extension BrowserViewController {
         guard let self = self else { return }
         self.addNTPTutorialPage()
       }
-      
+
       parentController.present(onboardingController, animated: false)
       isOnboardingOrFullScreenCalloutPresented = true
     }
   }
-  
+
   private func addNTPTutorialPage() {
     if showNTPEducation().isEnabled, let url = showNTPEducation().url {
       tabManager.addTab(
@@ -73,7 +73,7 @@ extension BrowserViewController {
         isPrivate: PrivateBrowsingManager.shared.isPrivateBrowsing)
     }
   }
-  
+
   func showNTPOnboarding() {
     if !topToolbar.inOverlayMode,
        topToolbar.currentURL == nil,
@@ -81,34 +81,34 @@ extension BrowserViewController {
       showPrivacyReportsOnboardingIfNeeded()
     }
   }
-  
+
   func showPrivacyReportsOnboardingIfNeeded() {
     if Preferences.PrivacyReports.ntpOnboardingCompleted.value || PrivateBrowsingManager.shared.isPrivateBrowsing {
       return
     }
-    
+
     let trackerCountThresholdForOnboarding = AppConstants.buildChannel.isPublic ? 250 : 20
     let trackerAdsTotal = BraveGlobalShieldStats.shared.adblock + BraveGlobalShieldStats.shared.trackingProtection
-    
+
     if trackerAdsTotal < trackerCountThresholdForOnboarding {
       return
     }
-    
+
     // If a controller is already presented (such as menu), do not show onboarding
     guard presentedViewController == nil else {
       return
     }
-    
+
     // We can only show this onboarding on the NTP
     guard let ntpController = tabManager.selectedTab?.newTabPageViewController,
           let statsFrame = ntpController.ntpStatsOnboardingFrame
     else {
       return
     }
-    
+
     // Project the statsFrame to the current frame
     let frame = view.convert(statsFrame, from: ntpController.view)
-    
+
     // Create a border view
     let borderView = UIView().then {
       let borderLayer = CAShapeLayer().then {
@@ -121,46 +121,46 @@ extension BrowserViewController {
       }
       $0.layer.addSublayer(borderLayer)
     }
-    
+
     view.addSubview(borderView)
     borderView.frame = frame
-    
+
     // Present the popover and mark onboarding as complete
     Preferences.PrivacyReports.ntpOnboardingCompleted.value = true
-    
+
     let controller = WelcomeNTPOnboardingController()
     controller.setText(details: Strings.Onboarding.ntpOnboardingPopOverTrackerDescription)
     controller.buttonText = Strings.PrivacyHub.onboardingButtonTitle
-    
+
     let popover = PopoverController(contentController: controller)
     popover.arrowDistance = 10.0
     popover.present(from: borderView, on: self) { [weak popover, weak self] in
       guard let popover = popover,
             let self = self
       else { return }
-      
+
       // Mask the shadow
       let maskFrame = self.view.convert(frame, to: popover.backgroundOverlayView)
       guard !maskFrame.isNull && !maskFrame.isInfinite && !maskFrame.isEmpty && !popover.backgroundOverlayView.bounds.isNull && !popover.backgroundOverlayView.bounds.isInfinite && !popover.backgroundOverlayView.bounds.isEmpty else {
         return
       }
-      
+
       guard maskFrame.origin.x.isFinite && maskFrame.origin.y.isFinite && maskFrame.size.width.isFinite && maskFrame.size.height.isFinite && maskFrame.size.width > 0 && maskFrame.size.height > 0 else {
         return
       }
-      
+
       let maskShape = CAShapeLayer().then {
         $0.fillRule = .evenOdd
         $0.fillColor = UIColor.white.cgColor
         $0.strokeColor = UIColor.clear.cgColor
       }
-      
+
       popover.backgroundOverlayView.layer.mask = maskShape
       popover.popoverDidDismiss = { _ in
         maskShape.removeFromSuperlayer()
         borderView.removeFromSuperview()
       }
-      
+
       controller.buttonTapped = { [weak self] in
         maskShape.removeFromSuperlayer()
         borderView.removeFromSuperview()
@@ -168,7 +168,7 @@ extension BrowserViewController {
           self?.openPrivacyReport()
         }
       }
-      
+
       DispatchQueue.main.async {
         maskShape.path = {
           let path = CGMutablePath()
@@ -182,51 +182,51 @@ extension BrowserViewController {
       }
     }
   }
-  
+
   func notifyTrackersBlocked(domain: String, trackerName: String, remainingTrackersCount: Int) {
     let controller = WelcomeBraveBlockedAdsController().then {
       $0.setData(domain: domain, trackerBlocked: trackerName, trackerCount: remainingTrackersCount)
     }
-    
+
     let popover = PopoverController(contentController: controller)
     popover.present(from: topToolbar.locationView.shieldsButton, on: self)
-    
+
     let pulseAnimationView = RadialPulsingAnimation(ringCount: 3)
     pulseAnimationView.present(
       icon: topToolbar.locationView.shieldsButton.imageView?.image,
       from: topToolbar.locationView.shieldsButton,
       on: popover,
       browser: self)
-    
+
     pulseAnimationView.animationViewPressed = { [weak self] in
       popover.dismissPopover() {
         self?.presentBraveShieldsViewController()
       }
     }
-    
+
     popover.popoverDidDismiss = { [weak self] _ in
       pulseAnimationView.removeFromSuperview()
-      
+
       DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
         guard let self = self else { return }
-        
+
         if self.shouldShowPlaylistOnboardingThisSession {
           self.showPlaylistOnboarding(tab: self.tabManager.selectedTab)
         }
       }
     }
   }
-  
+
   /// New Tab Page Education screen should load after onboarding is finished and user is on locale JP
   /// - Returns: A tuple which shows NTP Education is enabled and URL to be loaded
   func showNTPEducation() -> (isEnabled: Bool, url: URL?) {
     guard let url = BraveUX.ntpTutorialPageURL else {
       return (false, nil)
     }
-    
+
     return (Locale.current.regionCode == "JP", url)
   }
-  
+
   func completeOnboarding(_ controller: UIViewController) {
     Preferences.General.basicOnboardingCompleted.value = OnboardingState.completed.rawValue
     controller.dismiss(animated: true)
--- Client/Frontend/Browser/BrowserViewController/ProductNotifications/BrowserViewController+ProductNotification.swift
+++ Client/Frontend/Browser/BrowserViewController/ProductNotifications/BrowserViewController+ProductNotification.swift
@@ -13,9 +13,9 @@ private let log = Logger.browserLogger
 // MARK: - ProductNotification
 
 extension BrowserViewController {
-  
+
   // MARK: BenchmarkTrackerCountTier
-  
+
   enum BenchmarkTrackerCountTier: Int, Equatable, CaseIterable {
     case specialTier = 1000
     case newbieExclusiveTier = 5000
@@ -26,22 +26,22 @@ extension BrowserViewController {
     case primeTier = 250_000
     case grandTier = 500_000
     case legendaryTier = 1_000_000
-    
+
     var nextTier: BenchmarkTrackerCountTier? {
       guard let indexOfSelf = Self.allCases.firstIndex(where: { self == $0 }) else {
         return nil
       }
-      
+
       return Self.allCases[safe: indexOfSelf + 1]
     }
-    
+
     var value: Int {
       AppConstants.buildChannel.isPublic ? self.rawValue : self.rawValue / 100
     }
   }
-  
+
   // MARK: Internal
-  
+
   @objc func updateShieldNotifications() {
     // Adding slight delay here for 2 reasons
     // First the content Blocker stats will be updated in current tab
@@ -49,22 +49,22 @@ extension BrowserViewController {
     // Second the popover notification will be shown after page loaded
     DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
       guard let self = self else { return }
-      
+
       self.presentOnboardingAdblockNotifications()
       self.presentEducationalProductNotifications()
     }
   }
-  
+
   private func presentOnboardingAdblockNotifications() {
     if Preferences.DebugFlag.skipEduPopups == true { return }
-    
+
     var isAboutHomeUrl = false
     if let selectedTab = tabManager.selectedTab,
        let url = selectedTab.url,
        let internalURL = InternalURL(url) {
       isAboutHomeUrl = internalURL.isAboutHomeURL
     }
-    
+
     guard let selectedTab = tabManager.selectedTab,
           !Preferences.General.onboardingAdblockPopoverShown.value,
           !benchmarkNotificationPresented,
@@ -77,34 +77,32 @@ extension BrowserViewController {
     else {
       return
     }
-    
+
     let blockedRequests = selectedTab.contentBlocker.blockedRequests
-    
+
     if !blockedRequests.isEmpty,
        let url = selectedTab.url,
-       let firstBlockedUrl = blockedRequests.first
-    {
-      
+       let firstBlockedUrl = blockedRequests.first {
       let domain = url.baseDomain ?? url.host ?? url.schemelessAbsoluteString
       let trackerName = BlockedTrackerParser.parse(url: firstBlockedUrl, fallbackToDomainURL: true) ?? domain
-      
+
       notifyTrackersBlocked(
         domain: domain, trackerName: trackerName,
         remainingTrackersCount: blockedRequests.count - 1)
       Preferences.General.onboardingAdblockPopoverShown.value = true
     }
   }
-  
+
   private func presentEducationalProductNotifications() {
     if Preferences.DebugFlag.skipEduPopups == true { return }
-    
+
     var isAboutHomeUrl = false
     if let selectedTab = tabManager.selectedTab,
        let url = selectedTab.url,
        let internalURL = InternalURL(url) {
       isAboutHomeUrl = internalURL.isAboutHomeURL
     }
-    
+
     guard let selectedTab = tabManager.selectedTab,
           presentedViewController == nil,
           !benchmarkNotificationPresented,
@@ -117,31 +115,31 @@ extension BrowserViewController {
     else {
       return
     }
-    
+
     // Step 1: Load a video on a streaming site
     if !Preferences.ProductNotificationBenchmarks.videoAdBlockShown.value,
        selectedTab.url?.isVideoSteamingSiteURL == true {
-      
+
       notifyVideoAdsBlocked()
       Preferences.ProductNotificationBenchmarks.videoAdBlockShown.value = true
-      
+
       return
     }
-    
+
     // Step 2: Share Brave Benchmark Tiers
     let numOfTrackerAds = BraveGlobalShieldStats.shared.adblock + BraveGlobalShieldStats.shared.trackingProtection
     if numOfTrackerAds > benchmarkCurrentSessionAdCount + 20 {
       let existingTierList = BenchmarkTrackerCountTier.allCases.filter {
         Preferences.ProductNotificationBenchmarks.trackerTierCount.value < $0.value
       }
-      
+
       if !existingTierList.isEmpty {
         Preferences.ProductNotificationBenchmarks.trackerTierCount.value = numOfTrackerAds
-        
+
         guard let firstExistingTier = existingTierList.filter({ numOfTrackerAds > $0.value }).first else {
           return
         }
-        
+
         if numOfTrackerAds > firstExistingTier.value {
           notifyTrackerAdsCount(
             firstExistingTier.value,
@@ -149,7 +147,7 @@ extension BrowserViewController {
         }
       }
     }
-    
+
     // Step 3: Domain Specific Data Saved
     // Data Saved Pop-Over only exist in JP locale
     if Locale.current.regionCode == "JP" {
@@ -161,9 +159,9 @@ extension BrowserViewController {
         else {
           return
         }
-        
+
         notifyDomainSpecificDataSaved(domainFetchedSiteSavings)
-        
+
         DataSaved.insert(
           savedUrl: currentURL.absoluteString,
           amount: domainFetchedSiteSavings)
@@ -171,48 +169,48 @@ extension BrowserViewController {
       }
     }
   }
-  
+
   private func notifyVideoAdsBlocked() {
     let shareTrackersViewController = ShareTrackersController(trackingType: .videoAdBlock)
-    
+
     dismiss(animated: true)
     showBenchmarkNotificationPopover(controller: shareTrackersViewController)
   }
-  
+
   private func notifyTrackerAdsCount(_ count: Int, description: String) {
     let shareTrackersViewController = ShareTrackersController(trackingType: .trackerCountShare(count: count, description: description))
     dismiss(animated: true)
-    
+
     shareTrackersViewController.actionHandler = { [weak self] action in
       guard let self = self, action == .shareTheNewsTapped else { return }
-      
+
       self.showShareScreen()
     }
-    
+
     showBenchmarkNotificationPopover(controller: shareTrackersViewController)
   }
-  
+
   private func notifyDomainSpecificDataSaved(_ dataSaved: String) {
     let shareTrackersViewController = ShareTrackersController(trackingType: .domainSpecificDataSaved(dataSaved: dataSaved))
     dismiss(animated: true)
-    
+
     shareTrackersViewController.actionHandler = { [weak self] action in
       guard let self = self, action == .dontShowAgainTapped else { return }
-      
+
       Preferences.ProductNotificationBenchmarks.showingSpecificDataSavedEnabled.value = true
       self.dismiss(animated: true)
     }
-    
+
     showBenchmarkNotificationPopover(controller: shareTrackersViewController)
   }
-  
+
   private func showBenchmarkNotificationPopover(controller: (UIViewController & PopoverContentComponent)) {
     benchmarkNotificationPresented = true
-    
+
     let popover = PopoverController(contentController: controller, contentSizeBehavior: .autoLayout)
     popover.addsConvenientDismissalMargins = false
     popover.present(from: topToolbar.locationView.shieldsButton, on: self)
-    
+
     let pulseAnimation = RadialPulsingAnimation(ringCount: 3)
     pulseAnimation.present(
       icon: topToolbar.locationView.shieldsButton.imageView?.image,
@@ -223,20 +221,20 @@ extension BrowserViewController {
       pulseAnimation.removeFromSuperview()
     }
   }
-  
+
   // MARK: Actions
-  
+
   func showShareScreen() {
     dismiss(animated: true) {
       let globalShieldsActivityController =
       ShieldsActivityItemSourceProvider.shared.setupGlobalShieldsActivityController()
       globalShieldsActivityController.popoverPresentationController?.sourceView = self.view
-      
+
       globalShieldsActivityController.popoverPresentationController?.sourceRect = self.view.convert(
         self.topToolbar.locationView.shieldsButton.frame,
         from: self.topToolbar.locationView.shieldsButton.superview)
       globalShieldsActivityController.popoverPresentationController?.permittedArrowDirections = [.up]
-      
+
       self.present(globalShieldsActivityController, animated: true, completion: nil)
     }
   }
