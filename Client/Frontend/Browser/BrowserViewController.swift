e231cc7fbded3352ed297cf3818c5a2ede2eeaa2
Soner Yuksel <grkmyksl@gmail.com>
2022-06-03 01:24:57 -0400

    Removing PageZoomBar when display page zoom is disabled

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1908,13 +1908,15 @@ public class BrowserViewController: UIViewController, BrowserViewControllerDeleg
   }
   
   func displayPageZoom(visible: Bool) {
-    if !visible || pageZoomBar != nil {
-      alertStackView.arrangedSubviews.forEach({
-        $0.removeFromSuperview()
-      })
-      
+    if !visible || pageZoomBar != nil {     
+      pageZoomBar?.view.removeFromSuperview()
       updateViewConstraints()
       pageZoomBar = nil
+      
+      if let zoomBarView = pageZoomBar?.view {
+        alertStackView.removeArrangedSubview(zoomBarView)
+      }
+      
       return
     }
     
