32ead9f1faab08c9d82b2ce69c3103db2d6cbaab
Stephen Heaps <steaps.4@gmail.com>
2022-10-11 10:43:24 -0400

    Separate password entry field with biometrics into `PasswordEntryField` so it can be re-used for private key password protection

--- Sources/BraveWallet/PasswordEntryView.swift
+++ Sources/BraveWallet/PasswordEntryView.swift
@@ -16,6 +16,92 @@ struct PasswordEntryError: LocalizedError, Equatable {
   static let incorrectPassword = Self(message: Strings.Wallet.incorrectPasswordErrorMessage)
 }
 
+/// Field for entering wallet password, with optional biometrics support
+struct PasswordEntryField: View {
+  
+  /// The password being entered
+  @Binding var password: String
+  /// The error displayed under the password field
+  @Binding var error: PasswordEntryError?
+  /// If we should show the biometrics icon to allow biometics unlock (if available & password stored in keychain)
+  let shouldShowBiometrics: Bool
+  let keyringStore: KeyringStore
+  let onCommit: () -> Void
+  
+  @State private var attemptedBiometricsUnlock: Bool = false
+  
+  init(
+    password: Binding<String>,
+    error: Binding<PasswordEntryError?>,
+    shouldShowBiometrics: Bool,
+    keyringStore: KeyringStore,
+    onCommit: @escaping () -> Void
+  ) {
+    self._password = password
+    self._error = error
+    self.shouldShowBiometrics = shouldShowBiometrics
+    self.onCommit = onCommit
+    self.keyringStore = keyringStore
+  }
+  
+  private var biometricsIcon: Image? {
+    let context = LAContext()
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+      switch context.biometryType {
+      case .faceID:
+        return Image(systemName: "faceid")
+      case .touchID:
+        return Image(systemName: "touchid")
+      case .none:
+        return nil
+      @unknown default:
+        return nil
+      }
+    }
+    return nil
+  }
+  
+  private func fillPasswordFromKeychain() {
+    if let password = keyringStore.retrievePasswordFromKeychain() {
+      self.password = password
+      onCommit()
+    }
+  }
+  
+  var body: some View {
+    HStack {
+      SecureField(Strings.Wallet.passwordPlaceholder, text: $password, onCommit: onCommit)
+        .textContentType(.password)
+        .font(.subheadline)
+        .introspectTextField(customize: { tf in
+          tf.becomeFirstResponder()
+        })
+        .textFieldStyle(BraveValidatedTextFieldStyle(error: error))
+      if shouldShowBiometrics, keyringStore.isKeychainPasswordStored, let icon = biometricsIcon {
+        Button(action: fillPasswordFromKeychain) {
+          icon
+            .imageScale(.large)
+            .font(.headline)
+        }
+      }
+    }
+  }
+}
+
+#if DEBUG
+struct PasswordEntryField_Previews: PreviewProvider {
+  static var previews: some View {
+    PasswordEntryField(
+      password: Binding(get: { "" }, set: { _ in }),
+      error: Binding(get: { nil }, set: { _ in }),
+      shouldShowBiometrics: false,
+      keyringStore: .previewStore,
+      onCommit: {})
+  }
+}
+#endif
+
+/// View for entering a password with an title and message displayed, and optional biometrics
 struct PasswordEntryView: View {
   
   let keyringStore: KeyringStore
@@ -40,35 +126,10 @@ struct PasswordEntryView: View {
   
   @State private var password = ""
   @State private var error: PasswordEntryError?
-  @State private var attemptedBiometricsUnlock: Bool = false
   @Environment(\.presentationMode) @Binding private var presentationMode
   
   private var isPasswordValid: Bool {
     !password.isEmpty
-  }
-  
-  private var biometricsIcon: Image? {
-    let context = LAContext()
-    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
-      switch context.biometryType {
-      case .faceID:
-        return Image(systemName: "faceid")
-      case .touchID:
-        return Image(systemName: "touchid")
-      case .none:
-        return nil
-      @unknown default:
-        return nil
-      }
-    }
-    return nil
-  }
-  
-  private func fillPasswordFromKeychain() {
-    if let password = keyringStore.retrievePasswordFromKeychain() {
-      self.password = password
-      validate()
-    }
   }
   
   private func validate() {
@@ -88,7 +149,7 @@ struct PasswordEntryView: View {
     NavigationView {
       ScrollView(.vertical) {
         VStack(spacing: 36) {
-          Image("graphic-lock", bundle: .current)
+          Image("graphic-lock", bundle: .module)
             .accessibilityHidden(true)
           VStack {
             Text(message)
@@ -96,22 +157,13 @@ struct PasswordEntryView: View {
               .padding(.bottom)
               .multilineTextAlignment(.center)
               .fixedSize(horizontal: false, vertical: true)
-            HStack {
-              SecureField(Strings.Wallet.passwordPlaceholder, text: $password, onCommit: validate)
-                .textContentType(.password)
-                .font(.subheadline)
-                .introspectTextField(customize: { tf in
-                  tf.becomeFirstResponder()
-                })
-                .textFieldStyle(BraveValidatedTextFieldStyle(error: error))
-              if shouldShowBiometrics, keyringStore.isKeychainPasswordStored, let icon = biometricsIcon {
-                Button(action: fillPasswordFromKeychain) {
-                  icon
-                    .imageScale(.large)
-                    .font(.headline)
-                }
-              }
-            }
+            PasswordEntryField(
+              password: $password,
+              error: $error,
+              shouldShowBiometrics: shouldShowBiometrics,
+              keyringStore: keyringStore,
+              onCommit: validate
+            )
             .padding(.horizontal, 48)
           }
           Button(action: validate) {
@@ -128,14 +180,19 @@ struct PasswordEntryView: View {
         .navigationTitle(title)
       }
     }
-    .onAppear {
-      guard shouldShowBiometrics else { return }
-      DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [self] in
-        if !attemptedBiometricsUnlock && UIApplication.shared.isProtectedDataAvailable {
-          attemptedBiometricsUnlock = true
-          fillPasswordFromKeychain()
-        }
-      }
-    }
   }
 }
+
+#if DEBUG
+struct PasswordEntryView_Previews: PreviewProvider {
+  static var previews: some View {
+    PasswordEntryView(
+      keyringStore: .previewStore,
+      message: String.localizedStringWithFormat(
+        Strings.Wallet.removeAccountConfirmationMessage,
+        "Account 1"
+      ),
+      action: { _, _ in })
+  }
+}
+#endif
