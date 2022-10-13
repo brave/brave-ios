5fa5a7619629654e6a41f90444149d4fd688f1b6
Stephen Heaps <steaps.4@gmail.com>
2022-10-12 20:00:27 -0400

    Address PR feedback, feature flag `EditSiteConnectionView` to use ethereum coin type when `isSolanaDappsEnabled` flag is false.

--- Sources/BraveWallet/Crypto/CryptoView.swift
+++ Sources/BraveWallet/Crypto/CryptoView.swift
@@ -189,7 +189,7 @@ public struct CryptoView: View {
               EditSiteConnectionView(
                 keyringStore: keyringStore,
                 origin: origin,
-                coin: keyringStore.selectedAccount.coin,
+                coin: WalletDebugFlags.isSolanaDappsEnabled ? keyringStore.selectedAccount.coin : .eth,
                 onDismiss: { accounts in
                   handler(accounts)
                   dismissAction?()
