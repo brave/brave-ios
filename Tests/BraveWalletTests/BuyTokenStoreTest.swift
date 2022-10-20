2667e90e10eff96212658c8f8812af4e22f56898
Nuo Xu <nuoxu.nx@gmail.com>
2022-10-20 12:23:55 -0400

    added more tests for buyTokenStore.

--- Tests/BraveWalletTests/BuyTokenStoreTest.swift
+++ Tests/BraveWalletTests/BuyTokenStoreTest.swift
@@ -11,9 +11,11 @@ import BraveCore
 @testable import BraveWallet
 
 class BuyTokenStoreTests: XCTestCase {
-  private func setupServices() -> (BraveWallet.TestBlockchainRegistry, BraveWallet.TestJsonRpcService, BraveWallet.TestBraveWalletService, BraveWallet.TestAssetRatioService) {
+  private var cancellables: Set<AnyCancellable> = []
+  
+  private func setupServices(selectedNetwork: BraveWallet.NetworkInfo = .mockMainnet) -> (BraveWallet.TestBlockchainRegistry, BraveWallet.TestJsonRpcService, BraveWallet.TestBraveWalletService, BraveWallet.TestAssetRatioService) {
     let mockTokenList: [BraveWallet.BlockchainToken] = [
-      .init(contractAddress: "0x0d8775f648430679a709e98d2b0cb6250d2887ef", name: "Basic Attention Token", logo: "", isErc20: true, isErc721: false, symbol: "BAT", decimals: 18, visible: true, tokenId: "", coingeckoId: "", chainId: "", coin: .eth),
+      .init(contractAddress: "0x0d8775f648430679a709e98d2b0cb6250d2887ef", name: "Basic Attention Token", logo: "", isErc20: true, isErc721: false, symbol: "BAT", decimals: 18, visible: true, tokenId: "", coingeckoId: "", chainId: BraveWallet.MainnetChainId, coin: .eth),
       .init(contractAddress: "0xB8c77482e45F1F44dE1745F52C74426C631bDD52", name: "BNB", logo: "", isErc20: true, isErc721: false, symbol: "BNB", decimals: 18, visible: true, tokenId: "", coingeckoId: "", chainId: "", coin: .eth),
       .init(contractAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7", name: "Tether USD", logo: "", isErc20: true, isErc721: false, symbol: "USDT", decimals: 6, visible: true, tokenId: "", coingeckoId: "", chainId: "", coin: .eth),
       .init(contractAddress: "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85", name: "Ethereum Name Service", logo: "", isErc20: false, isErc721: true, symbol: "ENS", decimals: 1, visible: true, tokenId: "", coingeckoId: "", chainId: "", coin: .eth),
@@ -29,7 +31,7 @@ class BuyTokenStoreTests: XCTestCase {
     blockchainRegistry._onRampCurrencies = { $0(mockOnRampCurrencies) }
     
     let rpcService = BraveWallet.TestJsonRpcService()
-    rpcService._network = { $1(BraveWallet.NetworkInfo.mockMainnet) }
+    rpcService._network = { $1(selectedNetwork) }
     rpcService._addObserver = { _ in }
     
     let walletService = BraveWallet.TestBraveWalletService()
@@ -64,5 +66,46 @@ class BuyTokenStoreTests: XCTestCase {
     )
     XCTAssertEqual(store.selectedBuyToken?.symbol.lowercased(), BraveWallet.BlockchainToken.previewToken.symbol.lowercased())
   }
+  
+  func testBuyDisabledForTestNetwork() {
+    let (blockchainRegistry, rpcService, walletService, assetRatioService) = setupServices(selectedNetwork: .mockGoerli)
+    let store = BuyTokenStore(
+      blockchainRegistry: blockchainRegistry,
+      rpcService: rpcService,
+      walletService: walletService,
+      assetRatioService: assetRatioService,
+      prefilledToken: nil
+    )
+    
+    let isSelectedNetworkSupportedExpectation = expectation(description: "buyTokenStore-isSelectedNetworkSupported")
+    store.$isSelectedNetworkSupported
+      .dropFirst() // initial/default value
+      .sink { isSelectedNetworkSupported in
+        defer { isSelectedNetworkSupportedExpectation.fulfill() }
+        XCTAssertFalse(isSelectedNetworkSupported)
+      }
+      .store(in: &cancellables)
+    wait(for: [isSelectedNetworkSupportedExpectation], timeout: 2)
+  }
 
+  func testBuyEnabledForNonTestNetwork() {
+    let (blockchainRegistry, rpcService, walletService, assetRatioService) = setupServices(selectedNetwork: .mockMainnet)
+    let store = BuyTokenStore(
+      blockchainRegistry: blockchainRegistry,
+      rpcService: rpcService,
+      walletService: walletService,
+      assetRatioService: assetRatioService,
+      prefilledToken: nil
+    )
+    
+    let isSelectedNetworkSupportedExpectation = expectation(description: "buyTokenStore-isSelectedNetworkSupported")
+    store.$isSelectedNetworkSupported
+      .dropFirst() // initial/default value
+      .sink { isSelectedNetworkSupported in
+        defer { isSelectedNetworkSupportedExpectation.fulfill() }
+        XCTAssertTrue(isSelectedNetworkSupported)
+      }
+      .store(in: &cancellables)
+    wait(for: [isSelectedNetworkSupportedExpectation], timeout: 2)
+  }
 }
