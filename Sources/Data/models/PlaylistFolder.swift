d81982d0a0e5f9fb6bc9dae7b1f995397c8e9e7f
Soner Yuksel <grkmyksl@gmail.com>
2022-10-25 13:07:02 -0400

    Adding comments explaining why the merge type policy is changed

--- Sources/Data/models/PlaylistFolder.swift
+++ Sources/Data/models/PlaylistFolder.swift
@@ -119,6 +119,10 @@ final public class PlaylistFolder: NSManagedObject, CRUD, Identifiable {
       
       PlaylistFolder.reorderItems(context: context)
       
+      // Issue #6243 The policy change is added to prevent merge conflicts
+      // Occasionally saving context will give error
+      // Fatal error: Error saving DB: Error Domain=NSCocoaErrorDomain Code=133020 "Could not merge changes."
+      // Merge policy should change be changed to mergeByPropertyObjectTrumpMergePolicyType to prevent this
       let policy = context.mergePolicy
       context.mergePolicy = NSMergePolicy(merge: .mergeByPropertyObjectTrumpMergePolicyType)
       PlaylistFolder.saveContext(context)
