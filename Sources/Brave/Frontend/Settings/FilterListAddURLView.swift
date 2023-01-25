ec953e315caf3d04bcebcc23c3b5c0da16d0a63b
Jacob Sikorski <jacob.sikorski@gmail.com>
2023-01-25 17:04:27 +0100

    Fix #6841: Add custom filter list urls

---
+++ Sources/Brave/Frontend/Settings/FilterListAddURLView.swift
@@ -0,0 +1,111 @@
+// Copyright 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import SwiftUI
+import Strings
+import DesignSystem
+import BraveUI
+
+struct FilterListAddURLView: View {
+  @ObservedObject private var customFilterListStorage = CustomFilterListStorage.shared
+  @Environment(\.presentationMode) @Binding private var presentationMode
+  @State private var newURLInput: String = ""
+  @State private var errorMessage: String?
+  
+  private var textField: some View {
+    TextField(Strings.filterListsEnterFilterListURL, text: $newURLInput)
+      .onChange(of: newURLInput) { newValue in
+        errorMessage = nil
+      }
+      .keyboardType(.URL)
+      .textContentType(.URL)
+      .autocapitalization(.none)
+      .autocorrectionDisabled()
+  }
+  
+  var body: some View {
+    NavigationView {
+      List {
+        Section(content: {
+          VStack(alignment: .leading) {
+            textField
+              .submitLabel(SubmitLabel.done)
+            
+            if let errorMessage = errorMessage {
+              Text(errorMessage)
+                .font(.caption)
+                .foregroundColor(.red)
+            }
+          }
+        }, header: {
+          Text(Strings.customFilterListURL)
+        }, footer: {
+          VStack(alignment: .leading, spacing: 8) {
+            Text(Strings.addCustomFilterListDescription)
+            Text(LocalizedStringKey(Strings.addCustomFilterListWarning))
+          }.padding(.top, 8)
+        })
+      }
+      .listBackgroundColor(Color(UIColor.braveGroupedBackground))
+      .navigationTitle(Strings.customFilterList)
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbar {
+        ToolbarItemGroup(placement: .confirmationAction) {
+          Button(Strings.filterListsAdd) {
+            handleOnSubmit()
+          }.disabled(newURLInput.isEmpty)
+        }
+        
+        ToolbarItemGroup(placement: .cancellationAction) {
+          Button(Strings.CancelString) {
+            presentationMode.dismiss()
+          }
+        }
+      }
+    }.frame(idealWidth: 400, idealHeight: 400)
+  }
+  
+  private func handleOnSubmit() {
+    guard !newURLInput.isEmpty else { return }
+    guard let url = URL(string: newURLInput) else {
+      self.errorMessage = Strings.filterListAddInvalidURLError
+      return
+    }
+    guard url.scheme == "https" else {
+      self.errorMessage = Strings.filterListAddOnlyHTTPSAllowedError
+      return
+    }
+    guard !customFilterListStorage.filterListsURLs.contains(where: { filterListURL in
+      return filterListURL.setting.externalURL == url
+    }) else {
+      // Don't allow duplicates
+      self.presentationMode.dismiss()
+      return
+    }
+    
+    Task {
+      let customURL = FilterListCustomURL(
+        externalURL: url, isEnabled: true,
+        inMemory: !customFilterListStorage.persistChanges
+      )
+      
+      customFilterListStorage.filterListsURLs.append(customURL)
+      
+      await FilterListCustomURLDownloader.shared.startFetching(
+        filterListCustomURL: customURL
+      )
+      
+      self.presentationMode.dismiss()
+    }
+  }
+}
+
+#if DEBUG
+struct FilterListAddURLView_Previews: PreviewProvider {
+  static var previews: some View {
+    FilterListAddURLView()
+  }
+}
+#endif
