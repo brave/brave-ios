a00ff75f962d4d48ce261e56ed97ef637b450923
Brandon <JustBrandonT@gmail.com>
2023-03-08 21:16:44 -0500

    Added request handler

--- Sources/Brave/Frontend/Browser/Playlist/Managers & Cache/RangedResourceLoaderDelegate.swift
+++ Sources/Brave/Frontend/Browser/Playlist/Managers & Cache/RangedResourceLoaderDelegate.swift
@@ -39,31 +39,118 @@ private protocol MediaRequestProcessor {
   func processRequest(response: URLResponse?, data: Data, loadingRequest: AVAssetResourceLoadingRequest)
 }
 
-private class MediaRequest: NSObject, MediaRequestProcessor {
-  private var data = Data()
-  private let queue = DispatchQueue(label: "com.ranged.media-request", qos: .utility)
-  private var loadingRequest: AVAssetResourceLoadingRequest
-  private var response: URLResponse?
-  private let dateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-    dateFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss zzz"
-    return dateFormatter
-  }()
-  
+private class Session: NSObject, URLSessionTaskDelegate, URLSessionDataDelegate {
+  private let queue = DispatchQueue(label: "com.ranged.media-session", qos: .utility)
   private lazy var session: URLSession = {
     return URLSession(configuration: .ephemeral, delegate: self, delegateQueue: OperationQueue().then {
       $0.maxConcurrentOperationCount = 1
     })
   }()
   
-  deinit {
-    if !loadingRequest.isFinished && !loadingRequest.isCancelled {
-      loadingRequest.finishLoading(with: nil)
+  private class Request {
+    private(set) var processor: (Data, URLResponse?, Error?) -> Void
+    private(set) var response: URLResponse?
+    private(set) var data: Data
+    
+    init(processor: @escaping (Data, URLResponse?, Error?) -> Void) {
+      self.processor = processor
+      self.response = nil
+      self.data = Data()
     }
     
-    cancel()
+    func addData(_ data: Data) {
+      self.data.append(data)
+      processor(data, response, nil)
+    }
+    
+    func addResponse(_ response: URLResponse) {
+      self.response = response
+      processor(data, response, nil)
+    }
+    
+    func finish(with error: Error?) {
+      processor(Data(), nil, error)
+    }
   }
+  
+  private var tasks = [URLSessionTask: Request]()
+  
+  private override init() {
+    super.init()
+  }
+  
+  static let shared = Session()
+  
+  func removeAll() {
+    queue.sync { [weak self] in
+      guard let self = self else { return }
+      self.tasks.forEach({
+        $0.value.finish(with: nil)
+      })
+      
+      self.tasks = [:]
+    }
+  }
+  
+  func addRequest(_ request: URLRequest, processor: @escaping (Data, URLResponse?, Error?) -> Void) -> URLSessionDataTask? {
+    queue.sync { [weak self] in
+      guard let self = self else { return nil }
+      let task = self.session.dataTask(with: request)
+      self.tasks[task] = Request(processor: processor)
+      task.resume()
+      return task
+    }
+  }
+  
+  func removeRequest(_ task: URLSessionDataTask) {
+    queue.sync { [weak self] in
+      guard let self = self else { return }
+      self.tasks.removeValue(forKey: task)
+      task.cancel()
+    }
+  }
+  
+  func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+    queue.async { [weak self] in
+      guard let self = self else { return }
+      if let request = self.tasks.removeValue(forKey: task) {
+        request.finish(with: error)
+      }
+    }
+  }
+  
+  func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+    queue.async { [weak self] in
+      guard let self = self else { return }
+      if let request = self.tasks[dataTask] {
+        request.addData(data)
+      }
+    }
+  }
+  
+  func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+    queue.async { [weak self] in
+      guard let self = self else { return }
+      if let request = self.tasks[dataTask] {
+        request.addResponse(response)
+      }
+      
+      completionHandler(.allow)
+    }
+  }
+}
+
+private class MediaRequest: NSObject, MediaRequestProcessor {
+  private let queue = DispatchQueue(label: "com.ranged.media-request", qos: .utility)
+  private var loadingRequest: AVAssetResourceLoadingRequest
+  private var task: URLSessionDataTask?
+  
+  private let dateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    dateFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss zzz"
+    return dateFormatter
+  }()
   
   init(request: AVAssetResourceLoadingRequest) {
     self.loadingRequest = request
@@ -75,23 +162,54 @@ private class MediaRequest: NSObject, MediaRequestProcessor {
     let offset = loadingRequest.dataRequest?.requestedOffset ?? 0
     let length = Int64(loadingRequest.dataRequest?.requestedLength ?? 1)
     var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 60)
-//    var request = loadingRequest.request
-//    request.url = url
     request.setValue("bytes=\(offset)-\(offset + length - 1)", forHTTPHeaderField: "Range")
     loadingRequest.request.allHTTPHeaderFields?.forEach({
       request.setValue($0.value, forHTTPHeaderField: $0.key)
     })
     
+    if (url as NSURL).valueForQueryParameter(key: "clen") != nil, length != 2 {
+      request.url = (url as NSURL).replacingQueryParameter(key: "range", value: "\(offset)-\(offset + length - 1)")
+    }
+    
     super.init()
-    session.dataTask(with: request).resume()
+    
+    self.task = Session.shared.addRequest(request, processor: { [weak self] data, response, error in
+      guard let self = self else { return }
+      self.queue.async {
+        if response == nil {
+          self.finish(with: error)
+          return
+        }
+        
+        self.processRequest(response: response, data: data, loadingRequest: self.loadingRequest)
+      }
+    })
+  }
+  
+  deinit {
+    if !loadingRequest.isFinished && !loadingRequest.isCancelled {
+      loadingRequest.finishLoading(with: nil)
+    }
+    
+    if let task = task {
+      Session.shared.removeRequest(task)
+    }
   }
 
   func processRequest(response: URLResponse?, data: Data, loadingRequest: AVAssetResourceLoadingRequest) {
     fatalError("NOT IMPLEMENTED!")
   }
   
-  func cancel() {
-    session.invalidateAndCancel()
+  private func finish(with error: Error?) {
+    if loadingRequest.isFinished || loadingRequest.isCancelled {
+      return
+    }
+    
+    if let error = error {
+      loadingRequest.finishLoading(with: error)
+    } else {
+      loadingRequest.finishLoading()
+    }
   }
   
   func format(date: String) -> Date? {
@@ -100,46 +218,6 @@ private class MediaRequest: NSObject, MediaRequestProcessor {
   
   func isSame(as request: AVAssetResourceLoadingRequest) -> Bool {
     return loadingRequest === request
-  }
-}
-
-extension MediaRequest: URLSessionTaskDelegate {
-  func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-    queue.sync { [weak self] in
-      guard let self = self else { return }
-      if self.loadingRequest.isFinished || self.loadingRequest.isCancelled {
-        self.session.invalidateAndCancel()
-        return
-      }
-      
-      if let error = error {
-        self.loadingRequest.finishLoading(with: error)
-      } else {
-        self.loadingRequest.finishLoading()
-      }
-      
-      self.session.finishTasksAndInvalidate()
-    }
-  }
-}
-
-extension MediaRequest: URLSessionDataDelegate {
-  func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-    queue.sync { [weak self] in
-      guard let self = self else { return }
-      self.data = data
-      self.processRequest(response: self.response, data: self.data, loadingRequest: self.loadingRequest)
-    }
-  }
-  
-  func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
-    queue.sync { [weak self] in
-      guard let self = self else { return }
-      
-      self.response = response
-      self.processRequest(response: self.response, data: self.data, loadingRequest: self.loadingRequest)
-      completionHandler(.allow)
-    }
   }
 }
 
@@ -175,25 +253,30 @@ private class InfoRequest: MediaRequest {
         if let acceptRanges = response.value(forHTTPHeaderField: "Accept-Ranges") {
           infoRequest.isByteRangeAccessSupported = acceptRanges == "bytes"
         }
-      }
-
-      if let dataRequest = loadingRequest.dataRequest {
-        dataRequest.respond(with: data)
+        
+        loadingRequest.dataRequest?.respond(with: data)
+      } else {
+        let offset = Int(loadingRequest.dataRequest?.requestedOffset ?? 0)
+        let length = loadingRequest.dataRequest?.requestedLength ?? 0
+        
+        loadingRequest.dataRequest?.respond(with: data.subdata(in: offset..<(offset + length - 1)))
+        loadingRequest.finishLoading()
       }
     }
   }
 }
 
 private class DataRequest: MediaRequest {
+  private var totalData = 0
+  
   override func processRequest(response: URLResponse?, data: Data, loadingRequest: AVAssetResourceLoadingRequest) {
-    guard !loadingRequest.isFinished && !loadingRequest.isCancelled else { return }
+    totalData += data.count
+    guard !loadingRequest.isFinished && !loadingRequest.isCancelled, data.count > 0 else { return }
+    
     if let dataRequest = loadingRequest.dataRequest {
+      dataRequest.respond(with: data)
 
-      if data.count > 0 {
-        dataRequest.respond(with: data)
-      }
-
-      if data.count >= dataRequest.requestedLength {
+      if totalData >= dataRequest.requestedLength {
         loadingRequest.finishLoading()
       }
     }
@@ -212,6 +295,10 @@ class RangedResourceLoaderDelegate: NSObject {
     self.url = url
     self.streamURL = url.withMediaScheme
     super.init()
+  }
+  
+  static func destroy() {
+    Session.shared.removeAll()
   }
 }
 
@@ -271,9 +358,6 @@ extension RangedResourceLoaderDelegate: AVAssetResourceLoaderDelegate {
   }
   
   func resourceLoader(_ resourceLoader: AVAssetResourceLoader, didCancel loadingRequest: AVAssetResourceLoadingRequest) {
-    if let index = requests.firstIndex(where: { $0.isSame(as: loadingRequest) }) {
-      let request = requests.remove(at: index)
-      request.cancel()
-    }
+    requests.removeAll(where: { $0.isSame(as: loadingRequest) })
   }
 }
