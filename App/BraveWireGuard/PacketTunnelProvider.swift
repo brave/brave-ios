8e207d81fd83eb3c7261710defe62a9990f84c7e
Soner Yuksel <grkmyksl@gmail.com>
2023-03-20 15:29:45 -0400

    Embedding xframework and disabling list generation

--- App/BraveWireGuard/PacketTunnelProvider.swift
+++ App/BraveWireGuard/PacketTunnelProvider.swift
@@ -4,31 +4,8 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import NetworkExtension
+import GRDWireGuardKit
 
-class PacketTunnelProvider: NEPacketTunnelProvider {
-
-    override func startTunnel(options: [String: NSObject]?, completionHandler: @escaping (Error?) -> Void) {
-        // Add code here to start the process of connecting the tunnel.
-    }
-    
-    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-        // Add code here to start the process of stopping the tunnel.
-        completionHandler()
-    }
-    
-    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)?) {
-        // Add code here to handle the message.
-        if let handler = completionHandler {
-            handler(messageData)
-        }
-    }
-    
-    override func sleep(completionHandler: @escaping () -> Void) {
-        // Add code here to get ready to sleep.
-        completionHandler()
-    }
-    
-    override func wake() {
-        // Add code here to wake up.
-    }
+class PacketTunnelProvider: GRDPacketTunnelProvider {
+  
 }
