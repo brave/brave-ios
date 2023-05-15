036ba67ec3b3e14fdf60dcab17e47419ef536426
Nuo Xu <nuoxu.nx@gmail.com>
2023-05-14 23:47:04 -0400

    update SolanaTxManagerProxyExtensions to fetch estimated tx fee using TransactionInfo

--- Sources/BraveWallet/Extensions/SolanaTxManagerProxyExtensions.swift
+++ Sources/BraveWallet/Extensions/SolanaTxManagerProxyExtensions.swift
@@ -7,38 +7,6 @@ import BraveCore
 
 extension BraveWalletSolanaTxManagerProxy {
   
-  /// Fetches the estimatedTxFee for an array of transaction meta ids.
-  func estimatedTxFees(
-    chainId: String,
-    for transactionMetaIds: [String],
-    completion: @escaping ([String: UInt64]) -> Void
-  ) {
-    var estimatedTxFees: [String: UInt64] = [:]
-    let dispatchGroup = DispatchGroup()
-    transactionMetaIds.forEach { txMetaId in
-      dispatchGroup.enter()
-      estimatedTxFee(chainId, txMetaId: txMetaId) { fee, _, _ in
-        defer { dispatchGroup.leave() }
-        estimatedTxFees[txMetaId] = fee
-      }
-    }
-    dispatchGroup.notify(queue: .main) {
-      completion(estimatedTxFees)
-    }
-  }
-  
-  /// Fetches the estimatedTxFee for an array of transaction meta ids.
-  @MainActor func estimatedTxFees(
-    chainId: String,
-    for transactionMetaIds: [String]
-  ) async -> [String: UInt64] {
-    await withCheckedContinuation { continuation in
-      estimatedTxFees(chainId: chainId, for: transactionMetaIds) { fees in
-        continuation.resume(returning: fees)
-      }
-    }
-  }
-  
   /// Fetches the estimatedTxFee for an array of transactions
   @MainActor func estimatedTxFees(for transactions: [BraveWallet.TransactionInfo]
   ) async -> [String: UInt64] {
