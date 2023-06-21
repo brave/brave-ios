5a59fd0d2b734bc1d27204782c5da0c0df95c90a
Kyle Hickinson <git@kylehickinson.com>
2023-06-21 15:42:34 -0400

    Add background style option for spring buttons

--- Sources/BraveUI/SwiftUI/SpringButtonStyle.swift
+++ Sources/BraveUI/SwiftUI/SpringButtonStyle.swift
@@ -8,12 +8,49 @@ import SwiftUI
 /// Applies a scale effect when the user presses down with a spring animation attached to it
 public struct SpringButtonStyle: ButtonStyle {
   public var scale: CGFloat = 0.95
+  public var backgroundStyle: AnyShapeStyle?
+  
+  @State private var isPressed: Bool = false
+  @State private var pressDownTime: Date?
+  @State private var delayedTouchUpTask: Task<Void, Error>?
   
   public func makeBody(configuration: Configuration) -> some View {
     configuration.label
-      .scaleEffect(configuration.isPressed ? scale : 1.0)
-      .opacity(configuration.isPressed ? 0.95 : 1.0)
-      .animation(.spring(response: 0.3, dampingFraction: 0.8), value: configuration.isPressed)
+      .background {
+        if let backgroundStyle {
+          GeometryReader { proxy in
+            if isPressed {
+              Rectangle().fill(backgroundStyle)
+                .opacity(0.1)
+                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                .padding(-8)
+                .transition(.asymmetric(
+                  insertion: .opacity.animation(.linear(duration: 0.05)),
+                  removal: .opacity.animation(.interactiveSpring())
+                ))
+            }
+          }
+        }
+      }
+      .scaleEffect(isPressed ? scale : 1.0)
+      .opacity(isPressed ? 0.95 : 1.0)
+      .onChange(of: configuration.isPressed, perform: { value in
+        if value {
+          isPressed = value
+          pressDownTime = .now
+          delayedTouchUpTask?.cancel()
+        } else {
+          if let pressDownTime, case let delta = Date.now.timeIntervalSince(pressDownTime), delta < 0.1 {
+            delayedTouchUpTask = Task { @MainActor in
+              try await Task.sleep(nanoseconds: NSEC_PER_MSEC * UInt64((0.1 - delta) * 1000))
+              isPressed = value
+            }
+          } else {
+            isPressed = value
+          }
+        }
+      })
+      .animation(.spring(response: 0.3, dampingFraction: 0.8), value: isPressed)
   }
 }
 
@@ -21,7 +58,24 @@ extension ButtonStyle where Self == SpringButtonStyle {
   public static var spring: SpringButtonStyle {
     .init()
   }
-  public static func spring(scale: CGFloat = 0.95) -> SpringButtonStyle {
-    .init(scale: scale)
+  public static func spring(
+    scale: CGFloat = 0.95
+  ) -> SpringButtonStyle {
+    .init(scale: scale, backgroundStyle: nil)
+  }
+  public static func spring<S: ShapeStyle>(
+    scale: CGFloat = 0.95,
+    backgroundStyle: S
+  ) -> SpringButtonStyle {
+    .init(scale: scale, backgroundStyle: AnyShapeStyle(backgroundStyle))
   }
 }
+
+#if DEBUG
+struct SpringButtonStyle_PreviewProvider: PreviewProvider {
+  static var previews: some View {
+    Button("Text Test") { }
+      .buttonStyle(.spring(backgroundStyle: Color.black))
+  }
+}
+#endif
