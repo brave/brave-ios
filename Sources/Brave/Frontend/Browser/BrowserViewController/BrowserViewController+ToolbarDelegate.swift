876870cdbf3e510ad9b3824eb9cd79b6266ddcec
Soner YUKSEL <grkmyksl@gmail.com>
2023-08-02 16:58:06 -0400

    Ref #7686: Resetting Speech Recognizer properly after valid search conditions (#7820)
    Resetting Speech Recognizer properly after valid search conditions

--- Sources/Brave/Frontend/Browser/BrowserViewController/BrowserViewController+ToolbarDelegate.swift
+++ Sources/Brave/Frontend/Browser/BrowserViewController/BrowserViewController+ToolbarDelegate.swift
@@ -594,6 +594,8 @@ extension BrowserViewController: TopToolbarDelegate {
       if let query = searchQuery {
         self.submitSearchText(query)
       }
+      
+      self.speechRecognizer.clearSearch()
     }
   }
 
