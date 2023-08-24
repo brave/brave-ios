616bdb7abe50968750a60b04f3f7283a3925c2ff
Stephen Heaps <steaps.4@gmail.com>
2023-08-24 10:00:19 -0400

    Fix `Network Logo` and `Hide Unowned` NFT Filters & Display Settings toggles not resetting to defaults when `Reset` tapped

--- Sources/BraveWallet/Crypto/FiltersDisplaySettingsView.swift
+++ Sources/BraveWallet/Crypto/FiltersDisplaySettingsView.swift
@@ -199,7 +199,7 @@ struct FiltersDisplaySettingsView: View {
       .navigationBarTitleDisplayMode(.inline)
       .toolbar {
         ToolbarItem(placement: .navigationBarTrailing) {
-          Button(action: restoreToDefaults) {
+          Button(action: resetToDefaults) {
             Text(Strings.Wallet.settingsResetTransactionAlertButtonTitle)
               .fontWeight(.semibold)
               .foregroundColor(Color(uiColor: WalletV2Design.textInteractive))
@@ -380,12 +380,14 @@ struct FiltersDisplaySettingsView: View {
     .shadow(color: Color.black.opacity(0.04), radius: 16, x: 0, y: -8)
   }
   
-  func restoreToDefaults() {
-    self.groupBy = .none
+  func resetToDefaults() {
+    /// Assets are not grouped by default
+    self.groupBy = GroupBy(rawValue: Preferences.Wallet.groupByFilter.defaultValue) ?? .none
     // Fiat value in descending order (largest fiat to smallest) by default
-    self.sortOrder = .valueDesc
-    // Small balances shown by default
-    self.isHidingSmallBalances = false
+    self.sortOrder = SortOrder(rawValue: Preferences.Wallet.sortOrderFilter.defaultValue) ?? .valueDesc
+    self.isHidingSmallBalances = Preferences.Wallet.isHidingSmallBalancesFilter.defaultValue
+    self.isHidingUnownedNFTs = Preferences.Wallet.isHidingUnownedNFTsFilter.defaultValue
+    self.isShowingNFTNetworkLogo = Preferences.Wallet.isShowingNFTNetworkLogoFilter.defaultValue
     
     // All accounts selected by default
     self.accounts = self.accounts.map {
