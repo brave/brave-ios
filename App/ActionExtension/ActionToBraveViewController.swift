8c1ca4fb166bd9df705739224ae99907c68ebfed
Soner Yuksel <grkmyksl@gmail.com>
2024-01-18 17:54:02 -0500

    Adding generation of url in a separate path

--- App/ActionExtension/ActionToBraveViewController.swift
+++ App/ActionExtension/ActionToBraveViewController.swift
@@ -6,58 +6,13 @@
 import UIKit
 import MobileCoreServices
 import UniformTypeIdentifiers
-import os.log
 
 class ActionToBraveViewController: UIViewController {
-  private struct Scheme {
-    private enum SchemeType {
-      case url, query
-    }
-
-    private let type: SchemeType
-    private let urlOrQuery: String
-
-    init?(item: NSSecureCoding) {
-      if let text = item as? String {
-        urlOrQuery = text
-        type = .query
-      } else if let url = (item as? URL)?.absoluteString {
-        urlOrQuery = url
-        type = .url
-      } else {
-        return nil
-      }
-    }
-
-    var schemeUrl: URL? {
-      var components = URLComponents()
-      let queryItem: URLQueryItem
-
-      components.scheme = Bundle.main.infoDictionary?["BRAVE_URL_SCHEME"] as? String ?? "brave"
-
-      switch type {
-      case .url:
-        /// The first URL found within this String, or nil if no URL is found
-        guard let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue),
-              let match = detector.firstMatch(in: urlOrQuery, options: [], range: NSRange(location: 0, length: urlOrQuery.count)),
-              let range = Range(match.range, in: urlOrQuery),
-              let queryURL = URL(string: String(urlOrQuery[range]))?.absoluteString else {
-          
-          return nil
-        }
-        
-        components.host = "open-url"
-        queryItem = URLQueryItem(name: "url", value: queryURL)
-      case .query:
-        components.host = "search"
-        queryItem = URLQueryItem(name: "q", value: urlOrQuery)
-      }
-
-      components.queryItems = [queryItem]
-      return components.url
-    }
-  }
   
+  private enum SchemeType {
+    case url, query
+  }
+
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
       
@@ -65,15 +20,42 @@ class ActionToBraveViewController: UIViewController {
     for item in extensionContext?.inputItems as? [NSExtensionItem] ?? [] {
       for provider in item.attachments ?? [] {
         
-        provider.loadItem(forTypeIdentifier: provider.hasItemConformingToTypeIdentifier(UTType.url.identifier) ?
-                          UTType.url.identifier : UTType.text.identifier) { item, error in
-          DispatchQueue.main.async {
-            guard let item = item, let schemeUrl = Scheme(item: item)?.schemeUrl else {
-              self.done()
-              return
+        // Opening browser with search url
+        if provider.hasItemConformingToTypeIdentifier(UTType.text.identifier) {
+          provider.loadItem(forTypeIdentifier: UTType.text.identifier) { item, error in
+            DispatchQueue.main.async {
+              guard let item = item as? String,
+                    let schemeUrl = self.createURL(for: .query, with: item) else {
+                self.done()
+                return
+              }
+              
+              self.openBrowser(with: schemeUrl)
             }
-            
-            self.openBrowser(with: schemeUrl)
+          }
+        }
+    
+        // Opening browser with site
+        if provider.hasItemConformingToTypeIdentifier(UTType.url.identifier) {
+          provider.loadItem(forTypeIdentifier: UTType.url.identifier) { item, error in
+            DispatchQueue.main.async {
+              // The first URL found within item url absolute string
+              guard let item = (item as? URL)?.absoluteString,
+                    let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue),
+                    let match = detector.firstMatch(in: item, options: [], range: NSRange(location: 0, length: item.count)),
+                    let range = Range(match.range, in: item),
+                    let queryURL = URL(string: String(item[range]))?.absoluteString else {
+                self.done()
+                return
+              }
+
+              guard let schemeUrl = self.createURL(for: .url, with: queryURL) else {
+                self.done()
+                return
+              }
+              
+              self.openBrowser(with: schemeUrl)
+            }
           }
         }
         
@@ -83,22 +65,39 @@ class ActionToBraveViewController: UIViewController {
   }
 
   func done() {
-    // Return any edited content to the host app
-    // In this case empty
+    // Return any edited content to the host app, in this case empty
     extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+  }
+  
+  private func createURL(for schemeType: SchemeType, with value: String) -> URL? {
+    var queryItem: URLQueryItem
+    var components = URLComponents()
+    components.scheme = Bundle.main.infoDictionary?["BRAVE_URL_SCHEME"] as? String ?? "brave"
+
+    switch schemeType {
+    case .query:
+      queryItem = URLQueryItem(name: "q", value: value)
+      components.host = "search"
+    case .url:
+      queryItem = URLQueryItem(name: "url", value: value)
+      components.host = "open-url"
+    }
+    
+    components.queryItems = [queryItem]
+    return components.url
   }
     
   private func openBrowser(with url: URL) {
     var responder = self as UIResponder?
     
-    while let strongResponder = responder {
+    while let currentResponder = responder {
       let selector = sel_registerName("openURL:")
-      if strongResponder.responds(to: selector) {
+      if currentResponder.responds(to: selector) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
-          Thread.detachNewThreadSelector(selector, toTarget: strongResponder, with: (url as NSURL))
+          Thread.detachNewThreadSelector(selector, toTarget: currentResponder, with: (url as NSURL))
         }
       }
-      responder = strongResponder.next
+      responder = currentResponder.next
     }
 
     DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
