95e6fd14143ab748d987e8578411c63448aaac99
Brandon T <JustBrandonT@gmail.com>
2024-01-23 15:49:08 -0500

    Update Brave-Core to 1.64.x

--- App/iOS/Delegates/AppState.swift
+++ App/iOS/Delegates/AppState.swift
@@ -184,6 +184,9 @@ public class AppState {
     
     switches.append(.init(key: .init(rawValue: "force-fieldtrials"), value: "*BraveDebounceStudy/Enabled/"))
     switches.append(.init(key: .init(rawValue: "enable-features"), value: "BraveDebounce<BraveDebounceStudy"))
+    switches.append(.init(key: .init(rawValue: "--env-leo"), value: "staging"))
+    switches.append(.init(key: .init(rawValue: "--env-ai-chat.bsg"), value: "dev"))
+    switches.append(.init(key: .init(rawValue: "--env-ai-chat-premium.bsg"), value: "dev"))
                           
     // Initialize BraveCore
     return BraveCoreMain(userAgent: UserAgent.mobile, additionalSwitches: switches)
