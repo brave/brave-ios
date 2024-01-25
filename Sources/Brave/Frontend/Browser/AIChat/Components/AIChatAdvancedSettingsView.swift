8a26f7a3631d931fd753616507cfd5152722d39f
Soner Yuksel <grkmyksl@gmail.com>
2024-01-25 12:29:48 -0500

    Point of start in settings for leo advanced settings added

--- Sources/Brave/Frontend/Browser/AIChat/Components/AIChatAdvancedSettingsView.swift
+++ Sources/Brave/Frontend/Browser/AIChat/Components/AIChatAdvancedSettingsView.swift
@@ -8,23 +8,34 @@ import DesignSystem
 struct AIChatAdvancedSettingsView: View {
 
   @Environment(\.presentationMode) @Binding private var presentationMode
+  
+  var isModallyPresented: Bool
 
   var body: some View {
-    NavigationView {
-      VStack {
-        Text("Advanced Settings")
-          .tint(Color(braveSystemName: .textInteractive))
-          .font(.subheadline)
-      }
-      .navigationTitle("Settings")
-      .navigationBarTitleDisplayMode(.inline)
-      .toolbar {
-        ToolbarItemGroup(placement: .cancellationAction) {
-          Button("Close") {
-            presentationMode.dismiss()
+    if isModallyPresented {
+      NavigationView {
+        settingsView
+        .navigationTitle("Settings")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+          ToolbarItemGroup(placement: .cancellationAction) {
+            Button("Close") {
+              presentationMode.dismiss()
+            }
           }
         }
       }
+    } else {
+      settingsView
+        .navigationTitle("Settings")
+    }
+  }
+  
+  private var settingsView: some View {
+    VStack {
+      Text("Advanced Settings")
+        .tint(Color(braveSystemName: .textInteractive))
+        .font(.subheadline)
     }
   }
 }
