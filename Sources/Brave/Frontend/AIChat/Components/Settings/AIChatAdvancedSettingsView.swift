b4cf82b08dfda970057b23e1e17262fc0f122632
Soner Yuksel <grkmyksl@gmail.com>
2024-01-29 12:27:51 -0500

    Adding default model template and folders

--- Sources/Brave/Frontend/AIChat/Components/Settings/AIChatAdvancedSettingsView.swift
+++ Sources/Brave/Frontend/AIChat/Components/Settings/AIChatAdvancedSettingsView.swift
@@ -1,6 +1,7 @@
 // Copyright 2024 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import SwiftUI
 import BraveUI
@@ -50,7 +51,7 @@ struct AIChatAdvancedSettingsView: View {
         )
         
         NavigationLink {
-          // TODO: Add default model selection
+          AIChatDefaultModelView()
         } label: {
           LabelView(
             title: "Default model for new conversations",
