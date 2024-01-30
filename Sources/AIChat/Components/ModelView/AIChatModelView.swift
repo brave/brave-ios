8b1172b87341bcd096cc824f0b15e59caab2b523
Soner Yuksel <grkmyksl@gmail.com>
2024-01-30 13:57:36 -0500

    Speech Recognition is added

--- Sources/AIChat/Components/ModelView/AIChatModelView.swift
+++ Sources/AIChat/Components/ModelView/AIChatModelView.swift
@@ -1,9 +1,7 @@
-//
-//  File.swift
-//  
-//
-//  Created by Brandon on 2024-01-30.
-//
+// Copyright 2024 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Foundation
 import SwiftUI
