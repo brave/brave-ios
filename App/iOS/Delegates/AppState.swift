f589650a57819bafb0ca3897148c3502d317c0ba
Brandon <JustBrandonT@gmail.com>
2024-02-09 17:04:51 -0500

    Switch to Production for TF builds

--- App/iOS/Delegates/AppState.swift
+++ App/iOS/Delegates/AppState.swift
@@ -181,11 +181,11 @@ public class AppState {
     }
     switches.append(.init(key: .rewardsFlags, value: BraveRewards.Configuration.current().flags))
     
-    if !AppConstants.buildChannel.isPublic {
+    /*if !AppConstants.buildChannel.isPublic {
       switches.append(.init(key: .init(rawValue: "env-leo"), value: "staging"))
       switches.append(.init(key: .init(rawValue: "env-ai-chat.bsg"), value: "dev"))
       switches.append(.init(key: .init(rawValue: "env-ai-chat-premium.bsg"), value: "dev"))
-    }
+    }*/
                           
     // Initialize BraveCore
     return BraveCoreMain(userAgent: UserAgent.mobile, additionalSwitches: switches)
